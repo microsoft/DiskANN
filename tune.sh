dfbf2c310192d9cacfeb5e3592d0035c81e66f04
Ubuntu <harshasi@fatnode1.n14uw44gbsdu3cvfvcchcfucod.xx.internal.cloudapp.net>
2019-01-06 12:17:01 +0000

    added log for SIFT1M

--- tune.sh
+++ tune.sh
@@ -2,6 +2,7 @@ for K in 100; do
     for L in 64 128 256; do
 	for R in 64 128 256; do
 	    for C in 64 128 256; do
+		echo "\n"L${L} R${R} C${C} "\n"
 		./build/tests/test_nsg_index /mnt/SIFT1M/sift_base.fvecs /mnt/SIFT1M/sift_base.gr ${L} ${R} ${C} /mnt/SIFT1M/sift_base.nsg
 		if [ $? -eq 0 ]
 		then
