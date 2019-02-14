4cb3fecde4c6fa8b7b31771dbaaf32d17ccc54ec
Suhas Jayaram Subramanya <t-sujs@microsoft.com>
2019-02-14 12:38:16 +0000

    bfs fix

--- tests/nsg_bfs.cpp
+++ tests/nsg_bfs.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include "tsl/robin_map.h"
 #include "tsl/robin_set.h"
 
@@ -13,27 +14,36 @@ void load_nsg(const char *filename, VecVec &graph, unsigned &width,
   std::ifstream in(filename, std::ios::binary);
   in.read((char *) &width, sizeof(unsigned));
   in.read((char *) &ep_, sizeof(unsigned));
+  std::cout << "ep_ = " << ep_ << ", width = " << width << std::endl;
   // width=100;
   while (!in.eof()) {
     unsigned k;
     in.read((char *) &k, sizeof(unsigned));
+    assert(k <= width);
+    if(k == 0){
+      std::cerr << "Detached node - id= " << graph.size() << std::endl;
+    }
     if (in.eof())
       break;
     std::vector<unsigned> tmp(k);
     in.read((char *) tmp.data(), k * sizeof(unsigned));
+    for(auto &id : tmp){
+      if(id == 67635444u){
+        std::cout << "found in nbr list of node #" << graph.size() << std::endl; 
+      }
+    }
     graph.push_back(tmp);
   }
   std::cout << "Total #nodes = " << graph.size() << std::endl;
 }
 
-void nsg_bfs(const VecVec &nsg, const unsigned ep_, VecSet &bfs_order) {
+void nsg_bfs(const VecVec &nsg, const unsigned start_node, VecSet *bfs_order, bool* visited) {
   tsl::robin_set<unsigned> *cur_level = new tsl::robin_set<unsigned>();
   tsl::robin_set<unsigned> *prev_level = new tsl::robin_set<unsigned>();
-  prev_level->insert(ep_);
+  prev_level->insert(start_node);
+  visited[start_node] = true;
   unsigned level = 0;
-
-  // set visited array to 0
-  std::vector<bool> visited(nsg.size(), false);
+  unsigned nsg_size = nsg.size();
   while (true) {
     // clear state
     cur_level->clear();
@@ -41,6 +51,9 @@ void nsg_bfs(const VecVec &nsg, const unsigned ep_, VecSet &bfs_order) {
     // select candidates
     for (auto id : *prev_level) {
       for (const auto &nbr : nsg[id]) {
+        if(nbr >= nsg_size){
+          std::cerr << "invalid" << std::endl;
+        }
         if (!visited[nbr]) {
           cur_level->insert(nbr);
           visited[nbr] = true;
@@ -58,17 +71,12 @@ void nsg_bfs(const VecVec &nsg, const unsigned ep_, VecSet &bfs_order) {
     // create a new set
     tsl::robin_set<unsigned> add(cur_level->size());
     add.insert(cur_level->begin(), cur_level->end());
-    bfs_order.push_back(add);
+    bfs_order->push_back(add);
 
     // swap cur_level and prev_level, increment level
     prev_level->clear();
     std::swap(prev_level, cur_level);
     level++;
-  }
-
-  // assert(visited[i] == true) for all nodes
-  for (const auto val : visited) {
-    assert(val);
   }
 
   // cleanup
@@ -98,13 +106,41 @@ int main(int argc, char **argv) {
   }
 
   VecVec   nsg;
-  VecSet   bfs_order;
+  tsl::robin_map<unsigned, VecSet*>   bfs_orders;
+
   unsigned ep_, width;
   load_nsg(argv[1], nsg, width, ep_);
+  std::cout << "nsg.size() = " << nsg.size() << std::endl;
+  bool* visited = new bool[nsg.size()]();
+  std::fill(visited, visited + nsg.size(), false);
 
-  nsg_bfs(nsg, ep_, bfs_order);
+  unsigned start_node = ep_;
+  bool complete = false;
+  while(!complete){
+    VecSet *bfs_order = new VecSet();
+    std::cout << "Start node: " << start_node << std::endl;
+    nsg_bfs(nsg, start_node, bfs_order, visited);
+    bfs_orders.insert(std::make_pair(start_node, bfs_order));
 
-  average_degree(nsg, bfs_order);
+    complete = true;
+    for(unsigned idx = 0; idx < nsg.size(); idx++){
+      if (!visited[idx]){
+        complete = false;
+        start_node = idx;
+        break;
+      }
+    }
+  }
+
+  for(auto &k_v : bfs_orders){
+    std::cout << "Start node: " << k_v.first << std::endl;
+    average_degree(nsg, *k_v.second);
+  }
+
+  for(auto &k_v : bfs_orders){
+    delete bfs_orders[k_v.first];
+  }
+  delete[] visited;
 
   return 0;
 }
