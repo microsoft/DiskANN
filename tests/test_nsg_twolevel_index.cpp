25050155254839ff574ab7d0fc415455e9653e87
Harsha Vardhan Simhadri <harsha.v.simhadri@gmail.com>
2019-03-09 18:27:27 -0800

    flush changes

--- tests/test_nsg_twolevel_index.cpp
+++ tests/test_nsg_twolevel_index.cpp
@@ -10,127 +10,128 @@
 
 
 void load_data(
-    char* filename,
-    float*& data,
-    unsigned& num,
-    unsigned& dim) 
+	char* filename,
+	float*& data,
+	unsigned& num,
+	unsigned& dim)
 {  // load data with sift10K pattern
-  std::ifstream in(filename, std::ios::binary);
-  if (!in.is_open()) {
-    std::cout << "open file error" << std::endl;
-    exit(-1);
-  }
-  in.read((char*)&dim, 4);
-  in.seekg(0, std::ios::end);
-  std::ios::pos_type ss = in.tellg();
-  size_t fsize = (size_t)ss;
-  num = (unsigned)(fsize / (dim + 1) / 4);
-  data = new float[(size_t)num * (size_t)dim];
+	std::ifstream in(filename, std::ios::binary);
+	if (!in.is_open()) {
+		std::cout << "open file error" << std::endl;
+		exit(-1);
+	}
+	in.read((char*)&dim, 4);
+	in.seekg(0, std::ios::end);
+	std::ios::pos_type ss = in.tellg();
+	size_t fsize = (size_t)ss;
+	num = (unsigned)(fsize / (dim + 1) / 4);
+	data = new float[(size_t)num * (size_t)dim];
 
-  in.seekg(0, std::ios::beg);
-  for (size_t i = 0; i < num; i++) {
-    in.seekg(4, std::ios::cur);
-    in.read((char*)(data + i * dim), dim * 4);
-  }
-  in.close();
+	in.seekg(0, std::ios::beg);
+	for (size_t i = 0; i < num; i++) {
+		in.seekg(4, std::ios::cur);
+		in.read((char*)(data + i * dim), dim * 4);
+	}
+	in.close();
 }
 
 template <class T>
 void sample_data(
-    size_t dim,
-    T* src,
-    unsigned src_npoints,
-    T* dst, 
-    unsigned dst_npoints,
-    std::vector<unsigned>& picked_pts)
+	size_t dim,
+	T* src,
+	unsigned src_npoints,
+	T* dst,
+	unsigned dst_npoints,
+	std::vector<unsigned>& picked_pts)
 {
-    assert(picked_pts.size() == 0);
-    picked_pts.reserve(dst_npoints);
+	assert(picked_pts.size() == 0);
+	picked_pts.reserve(dst_npoints);
 
-    std::set<unsigned> picked;
-    std::random_device rd; 
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(0, src_npoints-1);
+	std::set<unsigned> picked;
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dis(0, src_npoints - 1);
 
-    while (picked.size() < dst_npoints) {
-        unsigned r = dis(gen);
-        if (picked.find(r) == picked.end())
-            picked.insert(r);
-    }
+	while (picked.size() < dst_npoints) {
+		unsigned r = dis(gen);
+		if (picked.find(r) == picked.end())
+			picked.insert(r);
+	}
 
-    for (auto p : picked) {
-        memcpy(((char*)dst) + picked_pts.size() * dim * sizeof(T),
-            ((char*)src) + p * dim * sizeof(T), dim * sizeof(T));
-        picked_pts.push_back(p);
-    }
+	for (auto p : picked) {
+		memcpy(((char*)dst) + picked_pts.size() * dim * sizeof(T),
+			((char*)src) + p * dim * sizeof(T), dim * sizeof(T));
+		picked_pts.push_back(p);
+	}
 }
 
-int main(int argc, char** argv) 
+int main(int argc, char** argv)
 {
-  if (argc != 9 && argc !=10) {
-    std::cout << argv[0] << " data_file nn_graph_path L R C NS B1 save_graph_file"
-              << std::endl;
-    exit(-1);
-  }
-  float alpha = 1;
-  if (argc == 10)  alpha = (float) atof(argv[9]);
-	
-  float *data_load = NULL;
-  unsigned points_num, dim;
-  load_data(argv[1], data_load, points_num, dim);
+	if (argc != 11 && argc != 10) {
+		std::cout << argv[0] << " data_file nn_graph_path L R C NS B1 small_gr big_gr"
+			<< std::endl;
+		exit(-1);
+	}
+	float alpha = 1;
+	if (argc == 11)  alpha = (float)atof(argv[10]);
 
-  std::cout << "Data loaded" << std::endl;
-  
-  std::string nn_graph_path(argv[2]);
-  unsigned L = (unsigned)atoi(argv[3]);
-  unsigned R = (unsigned)atoi(argv[4]);
-  unsigned C = (unsigned)atoi(argv[5]);
-  unsigned NS = (unsigned)atoi(argv[6]);
-  unsigned NR = (unsigned)atoi(argv[7]);
+	float *data_load = NULL;
+	unsigned points_num, dim;
+	load_data(argv[1], data_load, points_num, dim);
 
-  float *data_sampled = new float[(size_t)NS * dim]; 
-  std::vector<unsigned> picked_pts;
-  sample_data(dim, data_load, points_num, data_sampled, NS, picked_pts);  // Sample and copy before 
-  std::cout << "Data sampled" << std::endl;
-  
-  data_load = efanna2e::data_align(data_load, points_num, dim);
-  data_sampled = efanna2e::data_align(data_sampled, NS, dim);
-  std::cout << "File data and sample data aligned" << std::endl;
+	std::cout << "Data loaded" << std::endl;
 
-  efanna2e::IndexNSG small_index(dim, NS, efanna2e::L2, nullptr);
-  {
-    auto s = std::chrono::high_resolution_clock::now();
-    efanna2e::Parameters paras;
-    paras.Set<unsigned>("L", 4*L);
-    paras.Set<unsigned>("R", 4*R);
-    paras.Set<unsigned>("C", C);
-    paras.Set<float>("alpha", alpha);
-    std::cout << "Params set. Build start..." << std::endl;
-    small_index.BuildFromAlltoAll(NS, NR, data_sampled, paras);
-    auto e = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff = e - s;
-    std::cout << "Small indexing time: " << diff.count() << std::endl;
-  }
+	std::string nn_graph_path(argv[2]);
+	unsigned L = (unsigned)atoi(argv[3]);
+	unsigned R = (unsigned)atoi(argv[4]);
+	unsigned C = (unsigned)atoi(argv[5]);
+	unsigned NS = (unsigned)atoi(argv[6]);
+	unsigned NR = (unsigned)atoi(argv[7]);
 
-  efanna2e::IndexNSG index(dim, points_num, efanna2e::L2, nullptr);
-  {
-    auto s = std::chrono::high_resolution_clock::now();
-    efanna2e::Parameters paras;
-    paras.Set<unsigned>("L", L);
-    paras.Set<unsigned>("R", R);
-    paras.Set<unsigned>("C", C);
-    paras.Set<float>("alpha", alpha);
-    paras.Set<std::string>("nn_graph_path", nn_graph_path);
-    std::cout << "Params set. Build start..." << std::endl;
-    index.BuildFromSmall(
-		points_num, data_load, paras,
-		small_index, picked_pts);
-    auto e = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff = e - s;
-    std::cout << "Big indexing time: " << diff.count() << std::endl;
-  }
+	float *data_sampled = new float[(size_t)NS * dim];
+	std::vector<unsigned> picked_pts;
+	sample_data(dim, data_load, points_num, data_sampled, NS, picked_pts);  // Sample and copy before 
+	std::cout << "Data sampled" << std::endl;
 
-  index.Save(argv[8]);
+	data_load = efanna2e::data_align(data_load, points_num, dim);
+	data_sampled = efanna2e::data_align(data_sampled, NS, dim);
+	std::cout << "File data and sample data aligned" << std::endl;
 
-  return 0;
+	efanna2e::IndexNSG small_index(dim, NS, efanna2e::L2, nullptr);
+	{
+		auto s = std::chrono::high_resolution_clock::now();
+		efanna2e::Parameters paras;
+		paras.Set<unsigned>("L", 4 * L);
+		paras.Set<unsigned>("R", 4 * R);
+		paras.Set<unsigned>("C", C);
+		paras.Set<float>("alpha", alpha);
+		std::cout << "Params set. Build start..." << std::endl;
+		small_index.BuildFromAlltoAll(NS, NR, data_sampled, paras);
+		auto e = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> diff = e - s;
+		std::cout << "Small indexing time: " << diff.count() << std::endl;
+	}
+	index.Save(argv[8]);
+
+
+	efanna2e::IndexNSG index(dim, points_num, efanna2e::L2, nullptr);
+	{
+		auto s = std::chrono::high_resolution_clock::now();
+		efanna2e::Parameters paras;
+		paras.Set<unsigned>("L", L);
+		paras.Set<unsigned>("R", R);
+		paras.Set<unsigned>("C", C);
+		paras.Set<float>("alpha", alpha);
+		paras.Set<std::string>("nn_graph_path", nn_graph_path);
+		std::cout << "Params set. Build start..." << std::endl;
+		index.BuildFromSmall(
+			points_num, data_load, paras,
+			small_index, picked_pts);
+		auto e = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> diff = e - s;
+		std::cout << "Big indexing time: " << diff.count() << std::endl;
+	}
+	index.Save(argv[9]);
+
+	return 0;
 }
---
+++ tests/test_nsg_twolevel_search.cpp
@@ -0,0 +1,134 @@
+//
+// Created by 付聪 on 2017/6/21.
+//
+
+#include <efanna2e/index_nsg.h>
+#include <efanna2e/util.h>
+#include <omp.h>
+
+void load_data(char* filename, float*& data, unsigned& num,
+               unsigned& dim) {  // load data with sift10K pattern
+  std::ifstream in(filename, std::ios::binary);
+  if (!in.is_open()) {
+    std::cout << "open file error" << std::endl;
+    exit(-1);
+  }
+  in.read((char*) &dim, 4);
+  std::cout << "data dimension: " << dim << std::endl;
+  in.seekg(0, std::ios::end);
+  std::ios::pos_type ss = in.tellg();
+
+  size_t fsize = (size_t) ss;
+  num = (unsigned) (fsize / (dim + 1) / 4);
+  std::cout << "Reading " << num << " points" << std::endl;
+  data = new float[(size_t) num * (size_t) dim];
+
+  in.seekg(0, std::ios::beg);
+  for (size_t i = 0; i < num; i++) {
+    in.seekg(4, std::ios::cur);
+    in.read((char*) (data + i * dim), dim * 4);
+  }
+  in.close();
+}
+
+/*void save_result(char* filename, std::vector<std::vector<unsigned> >& results)
+{
+  std::ofstream out(filename, std::ios::binary | std::ios::out);
+
+  for (unsigned i = 0; i < results.size(); i++) {
+    unsigned GK = (unsigned) results[i].size();
+    out.write((char*) &GK, sizeof(unsigned));
+    out.write((char*) results[i].data(), GK * sizeof(unsigned));
+  }
+  out.close();
+}*/
+
+void save_result(char* filename, unsigned* results, unsigned nd, unsigned nr) {
+  std::ofstream out(filename, std::ios::binary | std::ios::out);
+
+  for (unsigned i = 0; i < nd; i++) {
+    out.write((char*) &nr, sizeof(unsigned));
+    out.write((char*) (results + i * nr), nr * sizeof(unsigned));
+  }
+  out.close();
+}
+
+int main(int argc, char** argv) {
+  if (argc != 9) {
+    std::cout << argv[0] << " data_file query_file nsg_path search_L search_K "
+                            "result_path BeamWidth nsg=1/efanna=0"
+              << std::endl;
+    exit(-1);
+  }
+  float*   data_load = NULL;
+  unsigned points_num, dim;
+  // load_data(argv[1], data_load, points_num, dim);
+  efanna2e::load_Tvecs<float>(argv[1], data_load, points_num, dim);
+  float*   query_load = NULL;
+  unsigned query_num, query_dim;
+  // load_data(argv[2], query_load, query_num, query_dim);
+  efanna2e::load_Tvecs<float>(argv[2], query_load, query_num, query_dim);
+  assert(dim == query_dim);
+  std::cout << "Base and query data loaded" << std::endl;
+
+  unsigned L = (unsigned) atoi(argv[4]);
+  unsigned K = (unsigned) atoi(argv[5]);
+  int      beam_width = atoi(argv[7]);
+  int nsg_check = atoi(argv[8]);
+
+  if (L < K) {
+    std::cout << "search_L cannot be smaller than search_K!" << std::endl;
+    exit(-1);
+  }
+
+  // data_load = efanna2e::data_align(data_load, points_num, dim);  // one must
+  // align the data before build
+  // query_load = efanna2e::data_align(query_load, query_num, query_dim);
+  // std::cout << "Data Aligned" << std::endl;
+
+  efanna2e::IndexNSG index(dim, points_num, efanna2e::L2, nullptr);
+  if(nsg_check == 1)
+	  index.Load(argv[3]);  // to load NSG
+  else
+	  index.Load_nn_graph(argv[3]);  // to load EFANNA
+  std::cout << "Index loaded" << std::endl;
+    
+  index.populate_start_points_bfs();
+//  std::cout << "Initialized starting points based on BFS" << std::endl;
+
+  efanna2e::Parameters paras;
+  paras.Set<unsigned>("L_search", L);
+  paras.Set<unsigned>("P_search", L);
+
+  auto s = std::chrono::high_resolution_clock::now();
+
+  long long total_hops = 0;
+  long long total_cmps = 0;
+  unsigned* res = new unsigned[(size_t) query_num * K];
+
+#pragma omp parallel for schedule(static, 1000)
+  for (unsigned i = 0; i < query_num; i++) {
+    auto ret = index.BeamSearch(query_load + i * dim, data_load, K, paras,
+                                res + ((size_t) i) * K, beam_width);
+// auto ret = index.Search(query_load + i * dim, data_load, K, paras,
+// tmp.data());
+
+#pragma omp atomic
+    total_hops += ret.first;
+#pragma omp atomic
+    total_cmps += ret.second;
+  }
+
+  auto                          e = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> diff = e - s;
+  std::cout << "search time: " << diff.count() << "\n";
+
+  std::cout << "Average hops: " << (float) total_hops / (float) query_num
+            << std::endl
+            << "Average cmps: " << (float) total_cmps / (float) query_num
+            << std::endl;
+
+  save_result(argv[6], res, query_num, K);
+
+  return 0;
+}
