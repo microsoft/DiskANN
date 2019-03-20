414826354a8a49ffbed472d827259969fd000df3
Suhas Jayaram Subramanya <t-sujs@microsoft.com>
2019-03-20 14:41:37 +0530

    added converters

--- tests/ivecs_to_bin_u8.cpp
+++ tests/ivecs_to_bin_u8.cpp
@@ -17,7 +17,7 @@ void block_convert(std::ifstream& reader, std::ofstream& writer,
 }
 
 int main(int argc, char** argv) {
-  if (argc != 4) {
+  if (argc != 3) {
     std::cout << argv[0] << " input_ivecs output_bin" << std::endl;
     exit(-1);
   }
