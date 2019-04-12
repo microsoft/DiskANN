b185ad079ea0601071dc9a0e2e3d03206338e3f0
ravishankar <rakri@microsoft.com>
2019-04-12 17:21:42 +0000

    sweep index now accepts fvecs or bvecs base file

--- tests/test_nsg_sweep_index.cpp
+++ tests/test_nsg_sweep_index.cpp
@@ -6,6 +6,12 @@
 #include <efanna2e/util.h>
 #include <omp.h>
 
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
+
 void load_data(char* filename, float*& data, unsigned& num,
                unsigned& dim) {  // load data with sift10K pattern
   std::ifstream in(filename, std::ios::binary);
@@ -27,6 +33,84 @@ void load_data(char* filename, float*& data, unsigned& num,
   }
   in.close();
 }
+
+void load_bvecs(const char* filename, float*& data, unsigned& num,
+                unsigned& dim) {
+  unsigned new_dim = 0;
+  char*    buf;
+  int      fd;
+  fd = open(filename, O_RDONLY);
+  if (!(fd > 0)) {
+    std::cerr << "Data file " << filename
+              << " not found. Program will stop now." << std::endl;
+    assert(false);
+  }
+  struct stat sb;
+  assert(fstat(fd, &sb) == 0);
+  off_t fileSize = sb.st_size;
+  assert(sizeof(off_t) == 8);
+
+  buf = (char*) mmap(NULL, fileSize, PROT_READ, MAP_PRIVATE, fd, 0);
+  assert(buf);
+  // size_t x=4;
+  uint32_t file_dim;
+  std::memcpy(&file_dim, buf, 4);
+  dim = file_dim;
+  if (new_dim == 0)
+    new_dim = dim;
+
+  if (new_dim < dim)
+    std::cout << "load_bvecs " << filename << ". Current Dimension: " << dim
+              << ". New Dimension: First " << new_dim << " columns. "
+              << std::flush;
+  else if (new_dim > dim)
+    std::cout << "load_bvecs " << filename << ". Current Dimension: " << dim
+              << ". New Dimension: " << new_dim
+              << " (added columns with 0 entries). " << std::flush;
+  else
+    std::cout << "load_bvecs " << filename << ". Dimension: " << dim << ". "
+              << std::flush;
+
+  float* zeros = new float[new_dim];
+  for (size_t i = 0; i < new_dim; i++)
+    zeros[i] = 0;
+
+  num = (unsigned) (fileSize / (dim + 4));
+  data = new float[(size_t) num * (size_t) new_dim];
+
+  std::cout << "# Points: " << num << ".." << std::flush;
+
+#pragma omp parallel for schedule(static, 65536)
+  for (size_t i = 0; i < num; i++) {
+    uint32_t row_dim;
+    char*    reader = buf + (i * (dim + 4));
+    std::memcpy((char*) &row_dim, reader, sizeof(uint32_t));
+    if (row_dim != dim)
+      std::cerr << "ERROR: row dim does not match" << std::endl;
+    std::memcpy(data + (i * new_dim), zeros, new_dim * sizeof(float));
+    if (new_dim > dim) {
+      //	std::memcpy(data + (i * new_dim), (reader + 4),
+      //		    dim * sizeof(float));
+      for (size_t j = 0; j < dim; j++) {
+        uint8_t cur;
+        std::memcpy((char*) &cur, (reader + 4 + j), sizeof(uint8_t));
+        data[i * new_dim + j] = (float) cur;
+      }
+    } else {
+      for (size_t j = 0; j < new_dim; j++) {
+        uint8_t cur;
+        std::memcpy((char*) &cur, (reader + 4 + j), sizeof(uint8_t));
+        data[i * new_dim + j] = (float) cur;
+        //	std::memcpy(data + (i * new_dim),
+        //(reader + 4), 		    new_dim * sizeof(float));
+      }
+    }
+  }
+  int val = munmap(buf, fileSize);
+  close(fd);
+  std::cout << "done." << std::endl;
+}
+
 int main(int argc, char** argv) {
   if (argc != 14) {
     std::cout << "Correct usage\n"
@@ -39,7 +123,18 @@ int main(int argc, char** argv) {
 
   float*   data_load = NULL;
   unsigned points_num, dim;
-  load_data(argv[1], data_load, points_num, dim);
+
+  std::string bvecs(".bvecs");
+  std::string base_file(argv[1]);
+  if (base_file.find(bvecs) == std::string::npos) {
+    //		std::cout << "Loading base set as fvecs" << std::endl;
+    load_data(argv[1], data_load, points_num, dim);
+  } else {
+    //		std::cout << "Loading training set as bvecs" << std::endl;
+    load_bvecs(argv[1], data_load, points_num, dim);
+  }
+
+  //  load_data(argv[1], data_load, points_num, dim);
   data_load = NSG::data_align(data_load, points_num, dim);
   std::cout << "Data loaded and aligned" << std::endl;
 
