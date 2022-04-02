6d749b4507e81b1bd5001d29d2d686144f6a9f42
Ubuntu <harsha.v.simhadri@gmail.com>
2022-04-02 17:15:01 +0000

    added more exception handling

--- src/ann_exception.cpp
+++ src/ann_exception.cpp
@@ -3,34 +3,39 @@
 
 #include "ann_exception.h"
 #include <sstream>
+#include <string>
 
 namespace diskann {
   ANNException::ANNException(const std::string& message, int errorCode)
-      : _errorCode(errorCode), _message(message), _funcSig(""), _fileName(""),
-        _lineNum(0) {
+      : std::runtime_error(message), _errorCode(errorCode) {
+  }
+
+  std::string package_string(const std::string& item_name,
+                             const std::string& item_val) {
+    return std::string("[") + item_name + ": " + std::string(item_val) +
+           std::string("]");
   }
 
   ANNException::ANNException(const std::string& message, int errorCode,
                              const std::string& funcSig,
                              const std::string& fileName, unsigned lineNum)
-      : ANNException(message, errorCode) {
-    _funcSig = funcSig;
-    _fileName = fileName;
-    _lineNum = lineNum;
+      : ANNException(
+            package_string(std::string("FUNC"), funcSig) +
+                package_string(std::string("FILE"), fileName) +
+                package_string(std::string("LINE"), std::to_string(lineNum)) +
+                message,
+            errorCode) {
   }
 
-  std::string ANNException::message() const {
-    std::stringstream sstream;
-
-    sstream << "Exception: " << _message;
-    if (_funcSig != "")
-      sstream << ". occurred at: " << _funcSig;
-    if (_fileName != "" && _lineNum != 0)
-      sstream << " defined in file: " << _fileName << " at line: " << _lineNum;
-    if (_errorCode != -1)
-      sstream << ". OS error code: " << std::hex << _errorCode;
-
-    return sstream.str();
+  FileException::FileException(const std::string& filename,
+                               std::system_error& e, const std::string& funcSig,
+                               const std::string& fileName,
+                               unsigned int       lineNum)
+      : ANNException(std::string(" While opening file ") + filename +
+                         std::string(", error code: ") +
+                         std::to_string(e.code().value()) + "  " +
+                         e.code().message(),
+                     e.code().value(), funcSig, fileName, lineNum) {
   }
 
 }  // namespace diskann
