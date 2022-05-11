2dc51a49653daf54fce0989e76ccad4a941539a0
ravishankar <rakri@microsoft.com>
2022-05-10 23:54:50 -0700

    Cleaned up (csine distance for int8 and float

--- include/cosine_similarity.h
+++ include/cosine_similarity.h
@@ -18,44 +18,6 @@
 
 extern bool Avx2SupportedCPU;
 
-namespace diskann {
-  template<typename T>
-  inline float compute_l2_norm(const T* vector, uint64_t ndims) {
-    float norm = std::numeric_limits<float>::epsilon();
-    for (uint64_t i = 0; i < ndims; i++) {
-      norm += (float) (vector[i] * vector[i]);
-    }
-    return std::sqrt(norm);
-  }
-
-  template<typename T>
-  inline float compute_cosine_similarity(const T* left, const T* right,
-                                         uint64_t ndims) {
-    float left_norm = compute_l2_norm<T>(left, ndims);
-    float right_norm = compute_l2_norm<T>(right, ndims);
-    float dot = 0.0f;
-    for (uint64_t i = 0; i < ndims; i++) {
-      dot += (float) (left[i] * right[i]);
-    }
-    float cos_sim = dot / (left_norm * right_norm);
-    return cos_sim;
-  }
-
-  inline std::vector<float> compute_cosine_similarity_batch(
-      const float* query, const unsigned* indices, const float* all_data,
-      const unsigned ndims, const unsigned npts) {
-    std::vector<float> cos_dists;
-    cos_dists.reserve(npts);
-
-    for (size_t i = 0; i < npts; i++) {
-      const float* point = all_data + (size_t)(indices[i]) * (size_t)(ndims);
-      cos_dists.push_back(
-          compute_cosine_similarity<float>(point, query, ndims));
-    }
-    return cos_dists;
-  }
-}  // namespace diskann
-
 #ifdef _WINDOWS
 // SIMD implementation of Cosine similarity. Taken from hnsw library.
 
