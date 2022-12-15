c05934c75d106778ef6c0309daf536f62fff3961
Bryan Tower <brtower@microsoft.com>
2022-12-15 08:24:45 -0800

    adding boost program args to the client test

--- tests/restapi/client.cpp
+++ tests/restapi/client.cpp
@@ -7,6 +7,8 @@
 #include <string>
 #include <cstdlib>
 #include <codecvt>
+#include <boost/program_options.hpp>
+
 
 #include <cpprest/http_client.h>
 #include <restapi/common.h>
@@ -16,10 +18,11 @@ using namespace web::http;
 using namespace web::http::client;
 
 using namespace diskann;
+namespace po = boost::program_options;
 
 template<typename T>
 void query_loop(const std::string& ip_addr_port, const std::string& query_file,
-                const unsigned nq, const unsigned Ls) {
+                const unsigned nq, const unsigned Ls, const unsigned k_value) {
   web::http::client::http_client client(U(ip_addr_port));
 
   T*     data;
@@ -31,7 +34,7 @@ void query_loop(const std::string& ip_addr_port, const std::string& query_file,
     web::http::http_request http_query(methods::POST);
     web::json::value        queryJson = web::json::value::object();
     queryJson[QUERY_ID_KEY] = i;
-    queryJson[K_KEY] = 10;
+    queryJson[K_KEY] = k_value;
     queryJson[L_KEY] = Ls;
     for (size_t i = 0; i < ndims; ++i) {
       queryJson[VECTOR_KEY][i] = web::json::value::number(vec[i]);
@@ -59,25 +62,54 @@ void query_loop(const std::string& ip_addr_port, const std::string& query_file,
 }
 
 int main(int argc, char* argv[]) {
-  if (argc != 6) {
-    std::cout << "Usage: client ip_addr:port data_type<float/int8/uint8> query_file "
-                 "num_queries Ls"
-              << std::endl;
-    exit(1);
-  }
+    std::string data_type, query_file, address;
+    uint32_t num_queries;
+    uint32_t l_search, k_value;
 
-  const std::string ip_addr_port(argv[1]);
-  const std::string typestring(argv[2]);
-  const std::string query_file(argv[3]);
-  unsigned          nq = atoi(argv[4]);
-  unsigned          Ls = atoi(argv[5]);
+    po::options_description desc{ "Arguments" };
+    try {
+        desc.add_options()("help,h", "Print information on arguments");
+        desc.add_options()("data_type",
+            po::value<std::string>(&data_type)->required(),
+            "data type <int8/uint8/float>");
+        desc.add_options()("address",
+            po::value<std::string>(&address)->required(),
+            "Web server address");
+        desc.add_options()("query_file",
+            po::value<std::string>(&query_file)->required(),
+            "File containing the queries to search");
+        desc.add_options()(
+            "num_queries,Q",
+            po::value<uint32_t>(&num_queries)->required(),
+            "Number of queries to search");
+        desc.add_options()(
+            "l_search",
+            po::value<uint32_t>(&l_search)->required(),
+            "Value of L");
+        desc.add_options()(
+            "k_value,K",
+            po::value<uint32_t>(&k_value)->default_value(10),
+            "Value of K (default 10)");
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        if (vm.count("help")) {
+            std::cout << desc;
+            return 0;
+        }
+        po::notify(vm);
+    }
+    catch (const std::exception& ex) {
+        std::cerr << ex.what() << std::endl;
+        return -1;
+    }
+    
 
-  if (typestring == std::string("float")) {
-    query_loop<float>(ip_addr_port, query_file, nq, Ls);
-  } else if (typestring == std::string("int8")) {
-    query_loop<int8_t>(ip_addr_port, query_file, nq, Ls);
-  } else if (typestring == std::string("uint8")) {
-    query_loop<uint8_t>(ip_addr_port, query_file, nq, Ls);
+  if (data_type == std::string("float")) {
+    query_loop<float>(address, query_file, num_queries, l_search, k_value);
+  } else if (data_type == std::string("int8")) {
+    query_loop<int8_t>(address, query_file, num_queries, l_search, k_value);
+  } else if (data_type == std::string("uint8")) {
+    query_loop<uint8_t>(address, query_file, num_queries, l_search, k_value);
   } else {
     std::cerr << "Unsupported type " << argv[2] << std::endl;
     return -1;
