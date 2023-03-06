fccc5081ba910557c74d8da767e2f9937c875080
NingyuanChen <chenningyuan008@hotmail.com>
2023-03-06 10:17:43 -0800

    Throw exception when calling load_pq_centroid_bin if EXEC_ENV_OLS is defined.

--- src/index.cpp
+++ src/index.cpp
@@ -1425,6 +1425,8 @@ namespace diskann {
                                        file_num_points, _num_pq_chunks,
                                        _num_pq_chunks);
 #ifdef EXEC_ENV_OLS
+      throw ANNException("load_pq_centroid_bin should not be called when EXEC_ENV_OLS is defined.",
+                         -1, __FUNCSIG__, __FILE__, __LINE__);
       //pq_table.load_pq_centroid_bin(files, pq_pivots_file.c_str(), _num_pq_chunks);
 #else
       pq_table.load_pq_centroid_bin(pq_pivots_file.c_str(), _num_pq_chunks);
