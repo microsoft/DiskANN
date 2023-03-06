31054ee1ee3b48f47ddda943d30954b774413d4c
REDMOND\patelyash <patelyash@microsoft.com>
2023-03-06 11:06:43 -0500

    minor fix to label generation + pr-test update to build and search mem_index with label data

--- tests/utils/generate_synthetic_labels.cpp
+++ tests/utils/generate_synthetic_labels.cpp
@@ -70,7 +70,12 @@ int main(int argc, char** argv) {
           outfile << j;
           label_written = true;
         }
+        if (!label_written) {
+          outfile << 0;
+          label_written = true;
+        }
       }
+
 
       if (i < num_points - 1) {
         outfile << '\n';
