df5277c02e3414bab70317d23ee5eb4f2715294e
patelyash <patelyash@microsoft.com>
2023-03-23 09:48:07 -0400

    fixing bug while searching disk index and univ label appears 0 times in label file + adding test for filtered search and build disk index

--- src/pq_flash_index.cpp
+++ src/pq_flash_index.cpp
@@ -580,9 +580,8 @@ namespace diskann {
   void PQFlashIndex<T, LabelT>::set_universal_label(const LabelT &label) {
     int32_t temp_filter_num = get_filter_number(label);
     if (temp_filter_num == -1) {
-      diskann::cout << "Error, could not find universal label. Exitting."
+      diskann::cout << "Error, could not find universal label."
                     << std::endl;
-      exit(-1);
     } else {
       _use_universal_label = true;
       _universal_filter_num = (_u32) temp_filter_num;
