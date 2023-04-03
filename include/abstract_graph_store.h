8d6b832aaf580da0c975fce5ed00311176564054
Gopal Srinivasa <gopalsr@microsoft.com>
2023-04-04 00:25:30 +0530

    DiskANN Builds with initial refactoring changes

--- include/abstract_graph_store.h
+++ include/abstract_graph_store.h
@@ -14,13 +14,18 @@ namespace diskann
 class AbstractGraphStore
 {
   public:
-    AbstractGraphStore(const size_t max_pts);
+    AbstractGraphStore(const size_t max_pts)
+        : _max_points(max_pts){}
 
     virtual int load(const std::string &index_path_prefix) = 0;
     virtual int store(const std::string &index_path_prefix) = 0;
 
     virtual void get_adj_list(const location_t i, std::vector<location_t> &neighbors) = 0;
     virtual void set_adj_list(const location_t i, std::vector<location_t> &neighbors) = 0;
+
+  private:
+    size_t _max_points;
+      
 };
 
 } // namespace diskann
