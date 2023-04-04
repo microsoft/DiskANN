99fc27a7274f37cadc6054fcf329b0a4b786f68e
harsha vardhan simhadri <harsha.v.simhadri@gmail.com>
2023-04-04 15:48:21 -0700

    added in-mem dynamic index class to python bindings

--- python/src/diskann_bindings.cpp
+++ python/src/diskann_bindings.cpp
@@ -19,6 +19,7 @@
 #endif
 
 #include "disk_utils.h"
+#include "index.h"
 #include "pq_flash_index.h"
 
 PYBIND11_MAKE_OPAQUE(std::vector<unsigned>);
@@ -98,7 +99,7 @@ template <class T> struct DiskANNIndex
         return 0;
     }
 
-    auto search(py::array_t<T, py::array::c_style | py::array::forcecast> &query, const uint64_t dim,
+    auto search(py::array_t<T, py::array::c_style | py::array::forcecast> &query,
                 const uint64_t knn, const uint64_t l_search, const uint64_t beam_width)
     {
         py::array_t<unsigned> ids(knn);
@@ -118,7 +119,7 @@ template <class T> struct DiskANNIndex
         return std::make_pair(ids, dists);
     }
 
-    auto batch_search(py::array_t<T, py::array::c_style | py::array::forcecast> &queries, const uint64_t dim,
+    auto batch_search(py::array_t<T, py::array::c_style | py::array::forcecast> &queries,
                       const uint64_t num_queries, const uint64_t knn, const uint64_t l_search,
                       const uint64_t beam_width, const int num_threads)
     {
@@ -130,7 +131,7 @@ template <class T> struct DiskANNIndex
         std::vector<uint64_t> u64_ids(knn * num_queries);
 
 #pragma omp parallel for schedule(dynamic, 1)
-        for (int64_t i = 0; i < num_queries; i++)
+        for (int64_t i = 0; i < (int64_t)num_queries; i++)
         {
             pq_flash_index->cached_beam_search(queries.data(i), knn, l_search, u64_ids.data() + i * knn,
                                                dists.mutable_data(i), beam_width);
@@ -145,6 +146,75 @@ template <class T> struct DiskANNIndex
     }
 };
 
+typedef uint32_t IdT;
+typedef uint32_t filterT;
+
+template <class T> struct DynamicInMemIndex
+{
+    Index<T, IdT, filterT> *_index;
+    const IndexWriteParameters write_params;
+
+    DynamicInMemIndex(Metric m, const size_t dim, const size_t max_points, const IndexWriteParameters &index_parameters,
+                      const IndexReadParameters &search_parameters, const bool concurrent_consolidate)
+        : write_params(index_parameters)
+    {
+        _index = new Index<T>(m, dim, max_points,
+                              true,              // dynamic_index
+                              index_parameters,  // used for insert
+                              search_parameters, // used for searching
+                              true,              // enable_tags
+                              concurrent_consolidate,
+                              false,  // pq_dist_build
+                              0,      // num_pq_chunks
+                              false); // use_opq = false
+    }
+
+    int insert(py::array_t<T, py::array::c_style | py::array::forcecast> &vector, const IdT id)
+    {
+        return _index->insert_point(vector.data(), id);
+    }
+
+    int mark_deleted(const IdT id)
+    {
+        return _index->lazy_delete(id);
+    }
+
+    auto search(py::array_t<T, py::array::c_style | py::array::forcecast> &query, const uint64_t knn,
+                const uint64_t l_search)
+    {
+        py::array_t<IdT> ids(knn);
+        py::array_t<float> dists(knn);
+        std::vector<T *> empty_vector;
+        _index->search_with_tags(query.data(), knn, l_search, ids.mutable_data(), dists.mutable_data(), empty_vector);
+        return std::make_pair(ids, dists);
+    }
+
+    auto batch_search(py::array_t<T, py::array::c_style | py::array::forcecast> &queries, const uint64_t num_queries,
+                      const uint64_t knn, const uint64_t l_search, const int num_threads)
+    {
+        py::array_t<unsigned> ids({num_queries, knn});
+        py::array_t<float> dists({num_queries, knn});
+        std::vector<T *> empty_vector;
+
+        omp_set_num_threads(num_threads);
+
+#pragma omp parallel for schedule(dynamic, 1)
+        for (int64_t i = 0; i < (int64_t)num_queries; i++)
+        {
+            _index->search_with_tags(queries.data(i), knn, l_search, ids.mutable_data(i) + i * knn,
+                                     dists.mutable_data(i), empty_vector);
+        }
+
+        return std::make_pair(ids, dists);
+    }
+
+    auto consolidate_delete()
+    {
+        return _index->consolidate_deletes(write_params);
+    }
+};
+
+
 PYBIND11_MODULE(_diskannpy, m)
 {
     m.doc() = "DiskANN Python Bindings";
@@ -158,6 +228,48 @@ PYBIND11_MODULE(_diskannpy, m)
         .value("L2", Metric::L2)
         .value("INNER_PRODUCT", Metric::INNER_PRODUCT)
         .export_values();
+    
+    py::class_<DynamicInMemIndex<float>>(m, "DiskANNDynamicInMemFloatIndex")
+        .def(py::init([](diskann::Metric metric, const size_t dim, const size_t max_points,
+                         const IndexWriteParameters &index_parameters, const IndexReadParameters &search_parameters,
+                         const bool concurrent_consolidate) {
+            return std::unique_ptr<DynamicInMemIndex<float>>(new DynamicInMemIndex<float>(
+                metric, dim, max_points, index_parameters, search_parameters, concurrent_consolidate));
+        }))
+        .def("search", &DynamicInMemIndex<float>::search, py::arg("query"), py::arg("knn"), py::arg("l_search"))
+        .def("batch_search", &DynamicInMemIndex<float>::batch_search, py::arg("queries"), py::arg("num_queries"),
+             py::arg("knn"), py::arg("l_search"), py::arg("num_threads"))
+        .def("insert", &DynamicInMemIndex<float>::insert, py::arg("vector"), py::arg("id"))
+        .def("mark_deleted", &DynamicInMemIndex<float>::mark_deleted, py::arg("id"))
+        .def("consolidate_delete", &DynamicInMemIndex<float>::consolidate_delete);
+
+    py::class_<DynamicInMemIndex<int8_t>>(m, "DiskANNDynamicInMemInt8Index")
+        .def(py::init([](diskann::Metric metric, const size_t dim, const size_t max_points,
+                         const IndexWriteParameters &index_parameters, const IndexReadParameters &search_parameters,
+                         const bool concurrent_consolidate) {
+            return std::unique_ptr<DynamicInMemIndex<int8_t>>(new DynamicInMemIndex<int8_t>(
+                metric, dim, max_points, index_parameters, search_parameters, concurrent_consolidate));
+         }))
+         .def("search", &DynamicInMemIndex<int8_t>::search, py::arg("query"), py::arg("knn"), py::arg("l_search"))
+         .def("batch_search", &DynamicInMemIndex<int8_t>::batch_search, py::arg("queries"), py::arg("num_queries"),
+              py::arg("knn"), py::arg("l_search"), py::arg("num_threads"))
+         .def("insert", &DynamicInMemIndex<int8_t>::insert, py::arg("vector"), py::arg("id"))
+         .def("mark_deleted", &DynamicInMemIndex<int8_t>::mark_deleted, py::arg("id"))
+         .def("consolidate_delete", &DynamicInMemIndex<int8_t>::consolidate_delete);
+
+     py::class_<DynamicInMemIndex<uint8_t>>(m, "DiskANNDynamicInMemUint8Index")
+         .def(py::init([](diskann::Metric metric, const size_t dim, const size_t max_points,
+                          const IndexWriteParameters &index_parameters, const IndexReadParameters &search_parameters,
+                          const bool concurrent_consolidate) {
+             return std::unique_ptr<DynamicInMemIndex<uint8_t>>(new DynamicInMemIndex<uint8_t>(
+                 metric, dim, max_points, index_parameters, search_parameters, concurrent_consolidate));
+         }))
+         .def("search", &DynamicInMemIndex<uint8_t>::search, py::arg("query"), py::arg("knn"), py::arg("l_search"))
+         .def("batch_search", &DynamicInMemIndex<uint8_t>::batch_search, py::arg("queries"), py::arg("num_queries"),
+              py::arg("knn"), py::arg("l_search"), py::arg("num_threads"))
+         .def("insert", &DynamicInMemIndex<uint8_t>::insert, py::arg("vector"), py::arg("id"))
+         .def("mark_deleted", &DynamicInMemIndex<uint8_t>::mark_deleted, py::arg("id"))
+         .def("consolidate_delete", &DynamicInMemIndex<uint8_t>::consolidate_delete);
 
     py::class_<DiskANNIndex<float>>(m, "DiskANNFloatIndex")
         .def(py::init([](diskann::Metric metric) {
@@ -166,9 +278,9 @@ PYBIND11_MODULE(_diskannpy, m)
         .def("cache_bfs_levels", &DiskANNIndex<float>::cache_bfs_levels, py::arg("num_nodes_to_cache"))
         .def("load_index", &DiskANNIndex<float>::load_index, py::arg("index_path_prefix"), py::arg("num_threads"),
              py::arg("num_nodes_to_cache"), py::arg("cache_mechanism") = 1)
-        .def("search", &DiskANNIndex<float>::search, py::arg("query"), py::arg("dim"), py::arg("knn"),
+        .def("search", &DiskANNIndex<float>::search, py::arg("query"), py::arg("knn"),
              py::arg("l_search"), py::arg("beam_width"))
-        .def("batch_search", &DiskANNIndex<float>::batch_search, py::arg("queries"), py::arg("dim"),
+        .def("batch_search", &DiskANNIndex<float>::batch_search, py::arg("queries"),
              py::arg("num_queries"), py::arg("knn"), py::arg("l_search"), py::arg("beam_width"), py::arg("num_threads"))
         .def(
             "build",
@@ -195,9 +307,9 @@ PYBIND11_MODULE(_diskannpy, m)
         .def("cache_bfs_levels", &DiskANNIndex<int8_t>::cache_bfs_levels, py::arg("num_nodes_to_cache"))
         .def("load_index", &DiskANNIndex<int8_t>::load_index, py::arg("index_path_prefix"), py::arg("num_threads"),
              py::arg("num_nodes_to_cache"), py::arg("cache_mechanism") = 1)
-        .def("search", &DiskANNIndex<int8_t>::search, py::arg("query"), py::arg("dim"), py::arg("knn"),
+        .def("search", &DiskANNIndex<int8_t>::search, py::arg("query"), py::arg("knn"),
              py::arg("l_search"), py::arg("beam_width"))
-        .def("batch_search", &DiskANNIndex<int8_t>::batch_search, py::arg("queries"), py::arg("dim"),
+        .def("batch_search", &DiskANNIndex<int8_t>::batch_search, py::arg("queries"),
              py::arg("num_queries"), py::arg("knn"), py::arg("l_search"), py::arg("beam_width"), py::arg("num_threads"))
         .def(
             "build",
@@ -222,9 +334,9 @@ PYBIND11_MODULE(_diskannpy, m)
         .def("cache_bfs_levels", &DiskANNIndex<uint8_t>::cache_bfs_levels, py::arg("num_nodes_to_cache"))
         .def("load_index", &DiskANNIndex<uint8_t>::load_index, py::arg("index_path_prefix"), py::arg("num_threads"),
              py::arg("num_nodes_to_cache"), py::arg("cache_mechanism") = 1)
-        .def("search", &DiskANNIndex<uint8_t>::search, py::arg("query"), py::arg("dim"), py::arg("knn"),
+        .def("search", &DiskANNIndex<uint8_t>::search, py::arg("query"), py::arg("knn"),
              py::arg("l_search"), py::arg("beam_width"))
-        .def("batch_search", &DiskANNIndex<uint8_t>::batch_search, py::arg("queries"), py::arg("dim"),
+        .def("batch_search", &DiskANNIndex<uint8_t>::batch_search, py::arg("queries"),
              py::arg("num_queries"), py::arg("knn"), py::arg("l_search"), py::arg("beam_width"), py::arg("num_threads"))
         .def(
             "build",
@@ -242,4 +354,5 @@ PYBIND11_MODULE(_diskannpy, m)
             py::arg("data_file_path"), py::arg("index_prefix_path"), py::arg("R"), py::arg("L"),
             py::arg("final_index_ram_limit"), py::arg("indexing_ram_limit"), py::arg("num_threads"),
             py::arg("pq_disk_bytes") = 0);
+
 }
