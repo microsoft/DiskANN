112d4fe4e9c8ce062b7c4fcfdff2d53534e603ce
Gopal Srinivasa <gopalsr@microsoft.com>
2023-04-18 23:14:21 +0530

    Fixed distance invocations

--- include/ann_exception.h
+++ include/ann_exception.h
@@ -11,24 +11,25 @@
 #define __FUNCSIG__ __PRETTY_FUNCTION__
 #endif
 
-namespace diskann
-{
+namespace diskann {
 
-class ANNException : public std::runtime_error
-{
-  public:
-    DISKANN_DLLEXPORT ANNException(const std::string &message, int errorCode);
-    DISKANN_DLLEXPORT ANNException(const std::string &message, int errorCode, const std::string &funcSig,
-                                   const std::string &fileName, uint32_t lineNum);
+class ANNException : public std::runtime_error {
+ public:
+  DISKANN_DLLEXPORT ANNException(const std::string &message, int errorCode);
+  DISKANN_DLLEXPORT ANNException(const std::string &message, int errorCode,
+                                 const std::string &funcSig,
+                                 const std::string &fileName, uint32_t lineNum);
 
-  private:
-    int _errorCode;
+ private:
+  int _errorCode;
 };
 
-class FileException : public ANNException
-{
-  public:
-    DISKANN_DLLEXPORT FileException(const std::string &filename, std::system_error &e, const std::string &funcSig,
-                                    const std::string &fileName, uint32_t lineNum);
+class FileException : public ANNException {
+ public:
+  DISKANN_DLLEXPORT FileException(const std::string &filename,
+                                  std::system_error &e,
+                                  const std::string &funcSig,
+                                  const std::string &fileName,
+                                  uint32_t lineNum);
 };
-} // namespace diskann
+}  // namespace diskann
