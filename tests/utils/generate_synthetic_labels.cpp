424e33052b04efd417f1b52c3a2f0967e0c5e175
David Kaczynski <dkaczynski@microsoft.com>
2023-05-03 16:27:46 -0400

    Refactor iterator and conditions for printing labels

--- tests/utils/generate_synthetic_labels.cpp
+++ tests/utils/generate_synthetic_labels.cpp
@@ -35,11 +35,10 @@ class ZipfDistribution
         for (uint32_t i{0}; i < num_points; i++)
         {
             bool label_written = false;
-            for (auto it = distribution_map.cbegin(), next_it = it; it != distribution_map.cend(); it = next_it)
+            for (auto it = distribution_map.cbegin(); it != distribution_map.cend(); it++)
             {
-                next_it++;
                 auto label_selection_probability = std::bernoulli_distribution(distribution_factor / (double)it->first);
-                if (label_selection_probability(rand_engine))
+                if (label_selection_probability(rand_engine) && distribution_map[it->first] > 0)
                 {
                     if (label_written)
                     {
@@ -49,10 +48,6 @@ class ZipfDistribution
                     label_written = true;
                     // remove label from map if we have used all labels
                     distribution_map[it->first] -= 1;
-                    if (distribution_map[it->first] == 0)
-                    {
-                        distribution_map.erase(it);
-                    }
                 }
             }
             if (!label_written)
