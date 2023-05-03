ae7cb61a99732bf9e8b6e138211643efb3f9a8a6
David Kaczynski <dkaczynski@microsoft.com>
2023-05-03 10:50:01 -0400

    Add bool param for building a graph of labeled data

--- include/parameters.h
+++ include/parameters.h
@@ -21,16 +21,17 @@ class IndexWriteParameters
     const uint32_t max_occlusion_size; // C
     const float alpha;
     const uint32_t num_threads;
+    const bool data_is_labeled;
     const uint32_t filter_list_size; // Lf
     const uint32_t num_frozen_points;
 
   private:
     IndexWriteParameters(const uint32_t search_list_size, const uint32_t max_degree, const bool saturate_graph,
                          const uint32_t max_occlusion_size, const float alpha, const uint32_t num_threads,
-                         const uint32_t filter_list_size, const uint32_t num_frozen_points)
+                         const bool data_is_labeled, const uint32_t filter_list_size, const uint32_t num_frozen_points)
         : search_list_size(search_list_size), max_degree(max_degree), saturate_graph(saturate_graph),
           max_occlusion_size(max_occlusion_size), alpha(alpha), num_threads(num_threads),
-          filter_list_size(filter_list_size), num_frozen_points(num_frozen_points)
+          data_is_labeled(data_is_labeled), filter_list_size(filter_list_size), num_frozen_points(num_frozen_points)
     {
     }
 
@@ -75,6 +76,12 @@ class IndexWriteParametersBuilder
         return *this;
     }
 
+    IndexWriteParametersBuilder &with_data_is_labeled(const bool data_is_labeled)
+    {
+        _data_is_labeled = data_is_labeled;
+        return *this;
+    }
+
     IndexWriteParametersBuilder &with_filter_list_size(const uint32_t filter_list_size)
     {
         _filter_list_size = filter_list_size == 0 ? _search_list_size : filter_list_size;
@@ -90,13 +97,14 @@ class IndexWriteParametersBuilder
     IndexWriteParameters build() const
     {
         return IndexWriteParameters(_search_list_size, _max_degree, _saturate_graph, _max_occlusion_size, _alpha,
-                                    _num_threads, _filter_list_size, _num_frozen_points);
+                                    _num_threads, _data_is_labeled, _filter_list_size, _num_frozen_points);
     }
 
     IndexWriteParametersBuilder(const IndexWriteParameters &wp)
         : _search_list_size(wp.search_list_size), _max_degree(wp.max_degree),
           _max_occlusion_size(wp.max_occlusion_size), _saturate_graph(wp.saturate_graph), _alpha(wp.alpha),
-          _filter_list_size(wp.filter_list_size), _num_frozen_points(wp.num_frozen_points)
+          _data_is_labeled(wp.data_is_labeled), _filter_list_size(wp.filter_list_size),
+          _num_frozen_points(wp.num_frozen_points)
     {
     }
     IndexWriteParametersBuilder(const IndexWriteParametersBuilder &) = delete;
@@ -109,6 +117,7 @@ class IndexWriteParametersBuilder
     bool _saturate_graph{defaults::SATURATE_GRAPH};
     float _alpha{defaults::ALPHA};
     uint32_t _num_threads{defaults::NUM_THREADS};
+    bool _data_is_labeled{defaults::DATA_IS_LABELED};
     uint32_t _filter_list_size{defaults::FILTER_LIST_SIZE};
     uint32_t _num_frozen_points{defaults::NUM_FROZEN_POINTS_STATIC};
 };
