65b8afa445788e357569008211825962a8f62dca
Yiyong Lin <yiyolin@microsoft.com>
2023-05-09 12:05:09 -0700

    refine

--- src/logger.cpp
+++ src/logger.cpp
@@ -22,7 +22,6 @@ std::function<void(LogLevel, const char *)> g_logger;
 void SetCustomLogger(std::function<void(LogLevel, const char *)> logger)
 {
     g_logger = logger;
-    cout << "Setted Custom Logger" << std::endl;
 }
 #endif
 
