e1a8d789ff0a4c8c2b1a6593ebadf4f3d9b8853d
litan1 <106347144+ltan1ms@users.noreply.github.com>
2023-07-26 13:12:56 -0700

    fix: use the passed in io_limit (#403)
    * fix: use the passed in io_limit
    
    * fix to be clang-formatted

--- src/pq_flash_index.cpp
+++ src/pq_flash_index.cpp
@@ -1122,8 +1122,8 @@ void PQFlashIndex<T, LabelT>::cached_beam_search(const T *query1, const uint64_t
                                                  QueryStats *stats)
 {
     LabelT dummy_filter = 0;
-    cached_beam_search(query1, k_search, l_search, indices, distances, beam_width, false, dummy_filter,
-                       std::numeric_limits<uint32_t>::max(), use_reorder_data, stats);
+    cached_beam_search(query1, k_search, l_search, indices, distances, beam_width, false, dummy_filter, io_limit,
+                       use_reorder_data, stats);
 }
 
 template <typename T, typename LabelT>
