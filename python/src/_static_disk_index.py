06fc0b719321341fe7cb02d07538a216bee9037c
Dax Pryce <daxpryce@microsoft.com>
2023-08-02 12:12:47 -0700

    Preparing for 0.6.0 diskannpy release (#407)
    * Some early staging for README updates and pyproject updates for a 0.6.0 release for diskannpy.
    
    * Trying to fix the CI badge to point toward main's latest build
    
    * Updating documentation for pdoc generation
    
    * Documentation updates. Tightened up the API to drop list support (there were entirely too many cases where it wouldn't work, and it's easier to just tell people to convert it themselves)
    
    * Some module reorganization to make pdoc actually display the docstrings for variables re-exported at the top level
    
    * A copy paste happened that shouldn't have.
    
    * Updating the apps to use the new 0.6.0 api
    
    * Addressing PR feedback
    
    * Some of the documentation changes didn't get made in both from_file or the constructor

--- python/src/_static_disk_index.py
+++ python/src/_static_disk_index.py
@@ -7,14 +7,16 @@ from typing import Optional
 
 import numpy as np
 
-from . import _diskannpy as _native_dap
-from ._common import (
+from . import (
     DistanceMetric,
     QueryResponse,
     QueryResponseBatch,
     VectorDType,
     VectorLike,
     VectorLikeBatch,
+)
+from . import _diskannpy as _native_dap
+from ._common import (
     _assert,
     _assert_2d,
     _assert_is_nonnegative_uint32,
@@ -29,6 +31,10 @@ __ALL__ = ["StaticDiskIndex"]
 
 
 class StaticDiskIndex:
+    """
+    A StaticDiskIndex is a disk-backed index that is not mutable.
+    """
+
     def __init__(
         self,
         index_directory: str,
@@ -41,29 +47,37 @@ class StaticDiskIndex:
         index_prefix: str = "ann",
     ):
         """
-        The diskannpy.DiskIndex represents our python API into the DiskANN Product Quantization Flash Index library.
+        ### Parameters
+        - **index_directory**: The directory containing the index files. This directory must contain the following
+            files:
+            - `{index_prefix}_sample_data.bin`
+            - `{index_prefix}_mem.index.data`
+            - `{index_prefix}_pq_compressed.bin`
+            - `{index_prefix}_pq_pivots.bin`
+            - `{index_prefix}_sample_ids.bin`
+            - `{index_prefix}_disk.index`
 
-        This class is responsible for searching a DiskANN disk index.
-
-        :param metric: One of {"l2", "mips"}. L2 is supported for all 3 vector dtypes, but MIPS is only
-            available for single point floating numbers (numpy.single)
-        :type metric: str
-        :param vector_dtype: The vector dtype this index will be exposing.
-        :type vector_dtype: Type[numpy.single], Type[numpy.byte], Type[numpy.ubyte]
-        :param index_directory: Path on disk where the disk index is stored
-        :type index_directory: str
-        :param num_threads: Number of threads used to load the index (>= 0)
-        :type num_threads: int
-        :param num_nodes_to_cache: Number of nodes to cache in memory (> -1)
-        :type num_nodes_to_cache: int
-        :param cache_mechanism: 1 -> use the generated sample_data.bin file for
-            the index to initialize a set of cached nodes, up to ``num_nodes_to_cache``, 2 -> ready the cache for up to
-            ``num_nodes_to_cache``, but do not initialize it with any nodes. Any other value disables node caching.
-        :param index_prefix: A shared prefix that all files in this index will use. Default is "ann".
-        :type index_prefix: str
-        :raises ValueError: If metric is not a valid metric
-        :raises ValueError: If vector dtype is not a supported dtype
-        :raises ValueError: If num_threads or num_nodes_to_cache is an invalid range.
+          It may also include the following optional files:
+            - `{index_prefix}_vectors.bin`: Optional. `diskannpy` builder functions may create this file in the
+              `index_directory` if the index was created from a numpy array
+            - `{index_prefix}_metadata.bin`: Optional. `diskannpy` builder functions create this file to store metadata
+            about the index, such as vector dtype, distance metric, number of vectors and vector dimensionality.
+            If an index is built from the `diskann` cli tools, this file will not exist.
+        - **num_threads**: Number of threads to use when searching this index. (>= 0), 0 = num_threads in system
+        - **num_nodes_to_cache**: Number of nodes to cache in memory (> -1)
+        - **cache_mechanism**: 1 -> use the generated sample_data.bin file for
+            the index to initialize a set of cached nodes, up to `num_nodes_to_cache`, 2 -> ready the cache for up to
+            `num_nodes_to_cache`, but do not initialize it with any nodes. Any other value disables node caching.
+        - **distance_metric**: A `str`, strictly one of {"l2", "mips", "cosine"}. `l2` and `cosine` are supported for all 3
+          vector dtypes, but `mips` is only available for single precision floats. Default is `None`. **This
+          value is only used if a `{index_prefix}_metadata.bin` file does not exist.** If it does not exist,
+          you are required to provide it.
+        - **vector_dtype**: The vector dtype this index has been built with. **This value is only used if a
+          `{index_prefix}_metadata.bin` file does not exist.** If it does not exist, you are required to provide it.
+        - **dimensions**: The vector dimensionality of this index. All new vectors inserted must be the same
+          dimensionality. **This value is only used if a `{index_prefix}_metadata.bin` file does not exist.** If it
+          does not exist, you are required to provide it.
+        - **index_prefix**: The prefix of the index files. Defaults to "ann".
         """
         index_prefix = _valid_index_prefix(index_directory, index_prefix)
         vector_dtype, metric, _, _ = _ensure_index_metadata(
@@ -71,7 +85,7 @@ class StaticDiskIndex:
             vector_dtype,
             distance_metric,
             1,  # it doesn't matter because we don't need it in this context anyway
-            dimensions
+            dimensions,
         )
         dap_metric = _valid_metric(metric)
 
@@ -79,12 +93,12 @@ class StaticDiskIndex:
         _assert_is_nonnegative_uint32(num_nodes_to_cache, "num_nodes_to_cache")
 
         self._vector_dtype = vector_dtype
-        if vector_dtype == np.single:
-            _index = _native_dap.StaticDiskFloatIndex
-        elif vector_dtype == np.ubyte:
+        if vector_dtype == np.uint8:
             _index = _native_dap.StaticDiskUInt8Index
-        else:
+        elif vector_dtype == np.int8:
             _index = _native_dap.StaticDiskInt8Index
+        else:
+            _index = _native_dap.StaticDiskFloatIndex
         self._index = _index(
             distance_metric=dap_metric,
             index_path_prefix=os.path.join(index_directory, index_prefix),
@@ -97,33 +111,22 @@ class StaticDiskIndex:
         self, query: VectorLike, k_neighbors: int, complexity: int, beam_width: int = 2
     ) -> QueryResponse:
         """
-        Searches the disk index by a single query vector in a 1d numpy array.
+        Searches the index by a single query vector.
 
-        numpy array dtype must match index.
-
-        :param query: 1d numpy array of the same dimensionality and dtype of the index.
-        :type query: numpy.ndarray
-        :param k_neighbors: Number of neighbors to be returned. If query vector exists in index, it almost definitely
-            will be returned as well, so adjust your ``k_neighbors`` as appropriate. (> 0)
-        :type k_neighbors: int
-        :param complexity: Size of list to use while searching. List size increases accuracy at the cost of latency. Must
-            be at least k_neighbors in size.
-        :type complexity: int
-        :param beam_width: The beamwidth to be used for search. This is the maximum number of IO requests each query
-            will issue per iteration of search code. Larger beamwidth will result in fewer IO round-trips per query,
-            but might result in slightly higher total number of IO requests to SSD per query. For the highest query
-            throughput with a fixed SSD IOps rating, use W=1. For best latency, use W=4,8 or higher complexity search.
-            Specifying 0 will optimize the beamwidth depending on the number of threads performing search, but will
-            involve some tuning overhead.
-        :type beam_width: int
-        :return: Returns a tuple of 1-d numpy ndarrays; the first including the indices of the approximate nearest
-            neighbors, the second their distances. These are aligned arrays.
+        ### Parameters
+        - **query**: 1d numpy array of the same dimensionality and dtype of the index.
+        - **k_neighbors**: Number of neighbors to be returned. If query vector exists in index, it almost definitely
+          will be returned as well, so adjust your ``k_neighbors`` as appropriate. Must be > 0.
+        - **complexity**: Size of distance ordered list of candidate neighbors to use while searching. List size
+          increases accuracy at the cost of latency. Must be at least k_neighbors in size.
+        - **beam_width**: The beamwidth to be used for search. This is the maximum number of IO requests each query
+          will issue per iteration of search code. Larger beamwidth will result in fewer IO round-trips per query,
+          but might result in slightly higher total number of IO requests to SSD per query. For the highest query
+          throughput with a fixed SSD IOps rating, use W=1. For best latency, use W=4,8 or higher complexity search.
+          Specifying 0 will optimize the beamwidth depending on the number of threads performing search, but will
+          involve some tuning overhead.
         """
-        _query = _castable_dtype_or_raise(
-            query,
-            expected=self._vector_dtype,
-            message=f"DiskIndex expected a query vector of dtype of {self._vector_dtype}"
-        )
+        _query = _castable_dtype_or_raise(query, expected=self._vector_dtype)
         _assert(len(_query.shape) == 1, "query vector must be 1-d")
         _assert_is_positive_uint32(k_neighbors, "k_neighbors")
         _assert_is_positive_uint32(complexity, "complexity")
@@ -151,40 +154,26 @@ class StaticDiskIndex:
         beam_width: int = 2,
     ) -> QueryResponseBatch:
         """
-        Searches the disk index for many query vectors in a 2d numpy array.
-
-        numpy array dtype must match index.
+        Searches the index by a batch of query vectors.
 
         This search is parallelized and far more efficient than searching for each vector individually.
 
-        :param queries: 2d numpy array, with column dimensionality matching the index and row dimensionality being the
-            number of queries intended to search for in parallel. Dtype must match dtype of the index.
-        :type queries: numpy.ndarray
-        :param k_neighbors: Number of neighbors to be returned. If query vector exists in index, it almost definitely
-            will be returned as well, so adjust your ``k_neighbors`` as appropriate. (> 0)
-        :type k_neighbors: int
-        :param complexity: Size of list to use while searching. List size increases accuracy at the cost of latency. Must
-            be at least k_neighbors in size.
-        :type complexity: int
-        :param num_threads: Number of threads to use when searching this index. (>= 0), 0 = num_threads in system
-        :type num_threads: int
-        :param beam_width: The beamwidth to be used for search. This is the maximum number of IO requests each query
-            will issue per iteration of search code. Larger beamwidth will result in fewer IO round-trips per query,
-            but might result in slightly higher total number of IO requests to SSD per query. For the highest query
-            throughput with a fixed SSD IOps rating, use W=1. For best latency, use W=4,8 or higher complexity search.
-            Specifying 0 will optimize the beamwidth depending on the number of threads performing search, but will
-            involve some tuning overhead.
-        :type beam_width: int
-        :return: Returns a tuple of 2-d numpy ndarrays; each row corresponds to the query vector in the same index,
-            and elements in row corresponding from 1..k_neighbors approximate nearest neighbors. The second ndarray
-            contains the distances, of the same form: row index will match query index, column index refers to
-            1..k_neighbors distance. These are aligned arrays.
+        ### Parameters
+        - **queries**: 2d numpy array, with column dimensionality matching the index and row dimensionality being the
+          number of queries intended to search for in parallel. Dtype must match dtype of the index.
+        - **k_neighbors**: Number of neighbors to be returned. If query vector exists in index, it almost definitely
+          will be returned as well, so adjust your ``k_neighbors`` as appropriate. Must be > 0.
+        - **complexity**: Size of distance ordered list of candidate neighbors to use while searching. List size
+          increases accuracy at the cost of latency. Must be at least k_neighbors in size.
+        - **num_threads**: Number of threads to use when searching this index. (>= 0), 0 = num_threads in system
+        - **beam_width**: The beamwidth to be used for search. This is the maximum number of IO requests each query
+          will issue per iteration of search code. Larger beamwidth will result in fewer IO round-trips per query,
+          but might result in slightly higher total number of IO requests to SSD per query. For the highest query
+          throughput with a fixed SSD IOps rating, use W=1. For best latency, use W=4,8 or higher complexity search.
+          Specifying 0 will optimize the beamwidth depending on the number of threads performing search, but will
+          involve some tuning overhead.
         """
-        _queries = _castable_dtype_or_raise(
-            queries,
-            expected=self._vector_dtype,
-            message=f"DiskIndex expected a query vector of dtype of {self._vector_dtype}"
-        )
+        _queries = _castable_dtype_or_raise(queries, expected=self._vector_dtype)
         _assert_2d(_queries, "queries")
         _assert_is_positive_uint32(k_neighbors, "k_neighbors")
         _assert_is_positive_uint32(complexity, "complexity")
--- python/tests/test_dynamic_memory_index.py
+++ python/tests/test_dynamic_memory_index.py
@@ -4,6 +4,7 @@
 import shutil
 import tempfile
 import unittest
+import warnings
 
 import diskannpy as dap
 import numpy as np
@@ -12,18 +13,19 @@ from sklearn.neighbors import NearestNeighbors
 
 
 def _calculate_recall(
-        result_set_tags: np.ndarray,
-        original_indices_to_tags: np.ndarray,
-        truth_set_indices: np.ndarray,
-        recall_at: int = 5
+    result_set_tags: np.ndarray,
+    original_indices_to_tags: np.ndarray,
+    truth_set_indices: np.ndarray,
+    recall_at: int = 5,
 ) -> float:
-
     found = 0
     for i in range(0, result_set_tags.shape[0]):
         result_set_set = set(result_set_tags[i][0:recall_at])
         truth_set_set = set()
         for knn_index in truth_set_indices[i][0:recall_at]:
-            truth_set_set.add(original_indices_to_tags[knn_index])  # mapped into our tag number instead
+            truth_set_set.add(
+                original_indices_to_tags[knn_index]
+            )  # mapped into our tag number instead
         found += len(result_set_set.intersection(truth_set_set))
     return found / (result_set_tags.shape[0] * recall_at)
 
@@ -32,12 +34,12 @@ class TestDynamicMemoryIndex(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls._test_matrix = [
-            build_random_vectors_and_memory_index(np.single, "l2", with_tags=True),
-            build_random_vectors_and_memory_index(np.ubyte, "l2", with_tags=True),
-            build_random_vectors_and_memory_index(np.byte, "l2", with_tags=True),
-            build_random_vectors_and_memory_index(np.single, "cosine", with_tags=True),
-            build_random_vectors_and_memory_index(np.ubyte, "cosine", with_tags=True),
-            build_random_vectors_and_memory_index(np.byte, "cosine", with_tags=True),
+            build_random_vectors_and_memory_index(np.float32, "l2", with_tags=True),
+            build_random_vectors_and_memory_index(np.uint8, "l2", with_tags=True),
+            build_random_vectors_and_memory_index(np.int8, "l2", with_tags=True),
+            build_random_vectors_and_memory_index(np.float32, "cosine", with_tags=True),
+            build_random_vectors_and_memory_index(np.uint8, "cosine", with_tags=True),
+            build_random_vectors_and_memory_index(np.int8, "cosine", with_tags=True),
         ]
         cls._example_ann_dir = cls._test_matrix[0][4]
 
@@ -58,9 +60,9 @@ class TestDynamicMemoryIndex(unittest.TestCase):
             index_vectors,
             ann_dir,
             vector_bin_file,
-            generated_tags
+            generated_tags,
         ) in self._test_matrix:
-            with self.subTest():
+            with self.subTest(msg=f"Testing dtype {dtype}"):
                 index = dap.DynamicMemoryIndex.from_file(
                     index_directory=ann_dir,
                     max_vectors=11_000,
@@ -82,7 +84,9 @@ class TestDynamicMemoryIndex(unittest.TestCase):
                     )
                     knn.fit(index_vectors)
                     knn_distances, knn_indices = knn.kneighbors(query_vectors)
-                    recall = _calculate_recall(diskann_neighbors, generated_tags, knn_indices, k)
+                    recall = _calculate_recall(
+                        diskann_neighbors, generated_tags, knn_indices, k
+                    )
                     self.assertTrue(
                         recall > 0.70,
                         f"Recall [{recall}] was not over 0.7",
@@ -96,9 +100,9 @@ class TestDynamicMemoryIndex(unittest.TestCase):
             index_vectors,
             ann_dir,
             vector_bin_file,
-            generated_tags
+            generated_tags,
         ) in self._test_matrix:
-            with self.subTest():
+            with self.subTest(msg=f"Testing dtype {dtype}"):
                 index = dap.DynamicMemoryIndex(
                     distance_metric="l2",
                     vector_dtype=dtype,
@@ -112,9 +116,6 @@ class TestDynamicMemoryIndex(unittest.TestCase):
 
                 k = 5
                 ids, dists = index.search(query_vectors[0], k_neighbors=k, complexity=5)
-                self.assertEqual(ids.shape[0], k)
-                self.assertEqual(dists.shape[0], k)
-                ids, dists = index.search(query_vectors[0].tolist(), k_neighbors=k, complexity=5)
                 self.assertEqual(ids.shape[0], k)
                 self.assertEqual(dists.shape[0], k)
 
@@ -176,7 +177,7 @@ class TestDynamicMemoryIndex(unittest.TestCase):
             index_vectors,
             ann_dir,
             vector_bin_file,
-            generated_tags
+            generated_tags,
         ) in self._test_matrix:
             with self.subTest():
                 index = dap.DynamicMemoryIndex(
@@ -211,8 +212,10 @@ class TestDynamicMemoryIndex(unittest.TestCase):
             index_vectors,
             ann_dir,
             vector_bin_file,
-            generated_tags
-        ) = build_random_vectors_and_memory_index(np.single, "l2", with_tags=True, index_prefix="not_ann")
+            generated_tags,
+        ) = build_random_vectors_and_memory_index(
+            np.single, "l2", with_tags=True, index_prefix="not_ann"
+        )
         good_ranges = {
             "distance_metric": "l2",
             "vector_dtype": np.single,
@@ -226,7 +229,7 @@ class TestDynamicMemoryIndex(unittest.TestCase):
             "filter_complexity": 10,
             "num_frozen_points": 10,
             "initial_search_complexity": 32,
-            "search_threads": 0
+            "search_threads": 0,
         }
 
         bad_ranges = {
@@ -248,7 +251,10 @@ class TestDynamicMemoryIndex(unittest.TestCase):
             kwargs = good_ranges.copy()
             kwargs[bad_value_key] = bad_ranges[bad_value_key]
             with self.subTest():
-                with self.assertRaises(ValueError, msg=f"expected to fail with parameter {bad_value_key}={bad_ranges[bad_value_key]}"):
+                with self.assertRaises(
+                    ValueError,
+                    msg=f"expected to fail with parameter {bad_value_key}={bad_ranges[bad_value_key]}",
+                ):
                     index = dap.DynamicMemoryIndex(saturate_graph=False, **kwargs)
 
     def test_value_ranges_search(self):
@@ -265,7 +271,7 @@ class TestDynamicMemoryIndex(unittest.TestCase):
                         initial_search_complexity=32,
                         max_vectors=10001,
                         complexity=64,
-                        graph_degree=32
+                        graph_degree=32,
                     )
                     index.search(query=np.array([], dtype=np.single), **kwargs)
 
@@ -291,7 +297,7 @@ class TestDynamicMemoryIndex(unittest.TestCase):
                         initial_search_complexity=32,
                         max_vectors=10001,
                         complexity=64,
-                        graph_degree=32
+                        graph_degree=32,
                     )
                     index.batch_search(
                         queries=np.array([[]], dtype=np.single), **kwargs
@@ -310,18 +316,26 @@ class TestDynamicMemoryIndex(unittest.TestCase):
             initial_search_complexity=32,
             max_vectors=10100,
             complexity=64,
-            graph_degree=32
+            graph_degree=32,
         )
         index.insert(np.array([1.0] * 10, dtype=np.single), 10099)
         index.insert(np.array([2.0] * 10, dtype=np.single), 10050)
         index.insert(np.array([3.0] * 10, dtype=np.single), 10053)
-        tags, distances = index.search(np.array([3.0] * 10, dtype=np.single), k_neighbors=5, complexity=64)
+        tags, distances = index.search(
+            np.array([3.0] * 10, dtype=np.single), k_neighbors=5, complexity=64
+        )
         self.assertIn(10053, tags)
         tags, distances = index.search(deletion_vector, k_neighbors=5, complexity=64)
-        self.assertIn(deletion_tag, tags, "deletion_tag should exist, as we have not deleted yet")
+        self.assertIn(
+            deletion_tag, tags, "deletion_tag should exist, as we have not deleted yet"
+        )
         index.mark_deleted(deletion_tag)
         tags, distances = index.search(deletion_vector, k_neighbors=5, complexity=64)
-        self.assertNotIn(deletion_tag, tags, "deletion_tag should not exist, as we have marked it for deletion")
+        self.assertNotIn(
+            deletion_tag,
+            tags,
+            "deletion_tag should not exist, as we have marked it for deletion",
+        )
         with tempfile.TemporaryDirectory() as tmpdir:
             index.save(tmpdir)
 
@@ -331,11 +345,96 @@ class TestDynamicMemoryIndex(unittest.TestCase):
                 initial_search_complexity=32,
                 max_vectors=10100,
                 complexity=64,
-                graph_degree=32
+                graph_degree=32,
             )
-            tags, distances = index2.search(deletion_vector, k_neighbors=5, complexity=64)
+            tags, distances = index2.search(
+                deletion_vector, k_neighbors=5, complexity=64
+            )
             self.assertNotIn(
                 deletion_tag,
                 tags,
-                "deletion_tag should not exist, as we saved and reloaded the index without it"
+                "deletion_tag should not exist, as we saved and reloaded the index without it",
             )
+
+    def test_inserts_past_max_vectors(self):
+        def _tiny_index():
+            return dap.DynamicMemoryIndex(
+                distance_metric="l2",
+                vector_dtype=np.float32,
+                dimensions=10,
+                max_vectors=2,
+                complexity=64,
+                graph_degree=32,
+                num_threads=16,
+            )
+
+
+        rng = np.random.default_rng(12345)
+
+        # insert 3 vectors and look for an exception
+        index = _tiny_index()
+        index.insert(rng.random(10, dtype=np.float32), 1)
+        index.insert(rng.random(10, dtype=np.float32), 2)
+        with self.assertRaises(RuntimeError):
+            index.insert(rng.random(10, dtype=np.float32), 3)
+
+        # insert 2 vectors, delete 1, and insert another and expect a warning
+        index = _tiny_index()
+        index.insert(rng.random(10, dtype=np.float32), 1)
+        index.insert(rng.random(10, dtype=np.float32), 2)
+        index.mark_deleted(2)
+        with self.assertWarns(UserWarning):
+            self.assertEqual(index._removed_num_vectors, 1)
+            self.assertEqual(index._num_vectors, 2)
+            index.insert(rng.random(10, dtype=np.float32), 3)
+            self.assertEqual(index._removed_num_vectors, 0)
+            self.assertEqual(index._num_vectors, 2)
+
+        # insert 3 batch and look for an exception
+        index = _tiny_index()
+        with self.assertRaises(RuntimeError):
+            index.batch_insert(
+                rng.random((3, 10), dtype=np.float32),
+                np.array([1,2,3], dtype=np.uint32)
+            )
+
+
+        # insert 2 batch, remove 1, add 1 and expect a warning, remove 1, insert 2 batch and look for an exception
+        index = _tiny_index()
+        index.batch_insert(
+            rng.random((2, 10), dtype=np.float32),
+            np.array([1,2], dtype=np.uint32)
+        )
+        index.mark_deleted(1)
+        with self.assertWarns(UserWarning):
+            index.insert(rng.random(10, dtype=np.float32), 3)
+        index.mark_deleted(2)
+        with self.assertRaises(RuntimeError):
+            index.batch_insert(rng.random((2,10), dtype=np.float32), np.array([4, 5], dtype=np.uint32))
+
+        # insert 1, remove it, add 2 batch, and expect a warning
+        index = _tiny_index()
+        index.insert(rng.random(10, dtype=np.float32), 1)
+        index.mark_deleted(1)
+        with self.assertWarns(UserWarning):
+            index.batch_insert(rng.random((2, 10), dtype=np.float32), np.array([10, 20], dtype=np.uint32))
+
+        # insert 2 batch, remove both, add 2 batch, and expect a warning
+        index = _tiny_index()
+        index.batch_insert(rng.random((2,10), dtype=np.float32), np.array([10, 20], dtype=np.uint32))
+        index.mark_deleted(10)
+        index.mark_deleted(20)
+        with self.assertWarns(UserWarning):
+            index.batch_insert(rng.random((2, 10), dtype=np.float32), np.array([15, 25], dtype=np.uint32))
+
+        # insert 2 batch, remove both, consolidate_delete, add 2 batch and do not expect warning
+        index = _tiny_index()
+        index.batch_insert(rng.random((2,10), dtype=np.float32), np.array([10, 20], dtype=np.uint32))
+        index.mark_deleted(10)
+        index.mark_deleted(20)
+        index.consolidate_delete()
+        with warnings.catch_warnings():
+            warnings.simplefilter("error")  # turns warnings into raised exceptions
+            index.batch_insert(rng.random((2, 10), dtype=np.float32), np.array([15, 25], dtype=np.uint32))
+
+
