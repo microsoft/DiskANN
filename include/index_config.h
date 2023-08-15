c4aec961115e56272c39b6f00ee2f279e690675c
yashpatel007 <patelyash1311@gmail.com>
2023-08-15 15:06:43 -0400

    resolving build failures

--- include/index_config.h
+++ include/index_config.h
@@ -199,8 +199,6 @@ class IndexConfigBuilder
         {
             if (_index_search_params != nullptr && _index_search_params->initial_search_list_size == 0)
                 throw ANNException("Error: please pass initial_search_list_size for building dynamic index.", -1);
-            if (_index_write_params == nullptr)
-                throw ANNException("Error: please pass index_write_params for dynamic index", -1);
         }
 
         return IndexConfig(_data_strategy, _graph_strategy, _metric, _dimension, _max_points, _num_pq_chunks,
