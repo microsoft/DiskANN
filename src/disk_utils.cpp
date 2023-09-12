bc92303b688bb4c509595baaa0d77ffeed804fc7
yashpatel007 <patelyash1311@gmail.com>
2023-09-12 13:16:56 -0400

    fixing build error

--- src/disk_utils.cpp
+++ src/disk_utils.cpp
@@ -637,7 +637,7 @@ int build_merged_vamana_index(std::string base_file, diskann::Metric compareMetr
         using TagT = uint32_t;
         diskann::Index<T, TagT, LabelT> _index(
             compareMetric, base_dim, base_num, std::make_shared<diskann::IndexWriteParameters>(paras), nullptr,
-            paras.num_frozen_points, false, false, false, build_pq_bytes > 0, build_pq_bytes, use_opq);
+            paras.num_frozen_points, false, false, false, build_pq_bytes > 0, build_pq_bytes, use_opq, use_filters);
         if (!use_filters)
             _index.build(base_file.c_str(), base_num);
         else
@@ -704,7 +704,7 @@ int build_merged_vamana_index(std::string base_file, diskann::Metric compareMetr
         diskann::Index<T> _index(compareMetric, shard_base_dim, shard_base_pts,
                                  std::make_shared<diskann::IndexWriteParameters>(low_degree_params), nullptr,
                                  low_degree_params.num_frozen_points, false, false, false, build_pq_bytes > 0,
-                                 build_pq_bytes, use_opq);
+                                 build_pq_bytes, use_opq, use_filters);
         if (!use_filters)
         {
             _index.build(shard_base_file.c_str(), shard_base_pts);
