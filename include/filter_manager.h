91eb6c005d9bd9149ab770d4ff0a5ff6209d9bac
yashpatel007 <patelyash1311@gmail.com>
2023-09-13 13:00:13 -0400

    adding DLLEXPORT to methods

--- include/filter_manager.h
+++ include/filter_manager.h
@@ -9,36 +9,36 @@ namespace diskann
 template <typename label_type> class FilterManager
 {
   public:
-    FilterManager(const size_t num_points);
+    DISKANN_DLLEXPORT FilterManager(const size_t num_points);
     ~FilterManager() = default;
 
     // needs some internal lock
-    bool detect_common_filters(uint32_t point_id, bool search_invocation,
-                               const std::vector<label_type> &incoming_labels);
+    DISKANN_DLLEXPORT bool detect_common_filters(uint32_t point_id, bool search_invocation,
+                                                 const std::vector<label_type> &incoming_labels);
 
-    const std::vector<label_type> &get_labels_by_point_id(const location_t point_id);
-    const tsl::robin_set<label_type> &get_all_label_set();
+    DISKANN_DLLEXPORT const std::vector<label_type> &get_labels_by_point_id(const location_t point_id);
+    DISKANN_DLLEXPORT const tsl::robin_set<label_type> &get_all_label_set();
     // Throws: out of range exception
-    void add_label_to_point(const location_t point_id, label_type label);
+    DISKANN_DLLEXPORT void add_label_to_point(const location_t point_id, label_type label);
     // returns internal mapping for given raw_label
-    label_type get_converted_label(const std::string &raw_label);
+    DISKANN_DLLEXPORT label_type get_converted_label(const std::string &raw_label);
 
-    void update_medoid_by_label(const label_type &label, const uint32_t new_medoid);
-    const uint32_t &get_medoid_by_label(const label_type &label);
-    bool label_has_medoid(const label_type &label);
-    void calculate_best_medoids(const size_t num_points_to_load, const uint32_t num_candidates);
+    DISKANN_DLLEXPORT void update_medoid_by_label(const label_type &label, const uint32_t new_medoid);
+    DISKANN_DLLEXPORT const uint32_t &get_medoid_by_label(const label_type &label);
+    DISKANN_DLLEXPORT bool label_has_medoid(const label_type &label);
+    DISKANN_DLLEXPORT void calculate_best_medoids(const size_t num_points_to_load, const uint32_t num_candidates);
 
     // TODO: in future we may accept a set or vector of universal labels
-    void set_universal_label(label_type universal_label);
-    const label_type get_universal_label() const;
+    DISKANN_DLLEXPORT void set_universal_label(label_type universal_label);
+    DISKANN_DLLEXPORT const label_type get_universal_label() const;
 
     // ideally takes raw label file and then genrate internal mapping and keep the info of mapping
-    size_t load_labels(const std::string &labels_file);
-    size_t load_medoids(const std::string &labels_to_medoid_file);
-    void load_label_map(const std::string &labels_map_file);
+    DISKANN_DLLEXPORT size_t load_labels(const std::string &labels_file);
+    DISKANN_DLLEXPORT size_t load_medoids(const std::string &labels_to_medoid_file);
+    DISKANN_DLLEXPORT void load_label_map(const std::string &labels_map_file);
 
-    void save_labels(const std::string &save_path_prefix, const size_t total_points);
-    void save_medoids(const std::string &save_path);
+    DISKANN_DLLEXPORT void save_labels(const std::string &save_path_prefix, const size_t total_points);
+    DISKANN_DLLEXPORT void save_medoids(const std::string &save_path);
 
   private:
     size_t _num_points;
