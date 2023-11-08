aae39a47915b76edd999ef15dbb4cd4872c6c4de
SMovaghati <119703730+SMovaghati@users.noreply.github.com>
2023-11-08 14:36:31 -0500

    Add openBLAS to CMake and add a temporary test project to use it (#484)
    * add openBLAS to cmake and add a temporary test project to use it
    * add a cmake configuration -DUSE_OPENBLAS that will get set to true only if it is a windows build.

---
+++ apps/test_open_blas.cpp
@@ -0,0 +1,26 @@
+#include <cblas.h>
+#include <stdio.h>
+#include <vector>
+#include <chrono>
+
+// A temporary test just to play with OpenBLAS
+int main(int argc, char **argv)
+{
+    printf("Compute alpha*A*B+beta*C using OpenBLAS cblas_dgemm \n");
+
+    int size = 100;
+    int m = size, k = size, n = size;
+    int lda = size, ldb = size, ldc = size;
+    double alpha = 1.0, beta = 2.0;
+
+    std::vector<double> A(m * k);
+    std::vector<double> B(k * n);
+    std::vector<double> C(m * n);
+
+    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, m, n, k, alpha, A.data(), lda, B.data(), ldb, beta, C.data(),
+                ldc);
+
+    printf("Completed Successfully. \n");
+
+    return 0;
+}
