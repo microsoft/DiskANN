cce54e50a493ed76a446b6d7f7d82193faafe115
Neelam Mahapatro <nmahapatro@microsoft.com>
2024-02-05 20:48:33 +0530

    parameter fix

--- python/src/builder.cpp
+++ python/src/builder.cpp
@@ -96,11 +96,11 @@ void build_memory_index(const diskann::Metric metric, const std::string &vector_
         }
         else
         {
-            // auto labels_file = prepare_filtered_label_map<T, TagT, LabelT>(index, index_output_path,
-            // filter_labels_file, universal_label);
+            auto labels_file = prepare_filtered_label_map<T, TagT, LabelT>(index, index_output_path, filter_labels_file,
+                                                                           universal_label);
             auto filter_params = diskann::IndexFilterParamsBuilder()
                                      .with_universal_label(universal_label)
-                                     .with_label_file(filter_labels_file)
+                                     .with_label_file(labels_file)
                                      .with_save_path_prefix(index_output_path)
                                      .build();
             index.build_filtered_index(vector_bin_path.c_str(), data_num, filter_params, tags);
@@ -114,9 +114,8 @@ void build_memory_index(const diskann::Metric metric, const std::string &vector_
         }
         else
         {
-            // auto labels_file = prepare_filtered_label_map<T, TagT, LabelT>(index, index_output_path,
-            // filter_labels_file,
-            //  universal_label);
+            auto labels_file = prepare_filtered_label_map<T, TagT, LabelT>(index, index_output_path, filter_labels_file,
+                                                                           universal_label);
             auto filter_params = diskann::IndexFilterParamsBuilder()
                                      .with_universal_label(universal_label)
                                      .with_label_file(filter_labels_file)
