07e1dfb4dd947e413adbb34baa26c8ed112f042d
Gopal Srinivasa <gopalsr@microsoft.com>
2024-07-11 13:12:23 +0530

    Fixed issue with code hanging when num_threads == 1

--- src/pq_flash_index.cpp
+++ src/pq_flash_index.cpp
@@ -136,6 +136,7 @@ void PQFlashIndex<T, LabelT>::setup_thread_data(uint64_t nthreads, uint64_t visi
             this->_thread_data.push(data);
         }
     }
+    this->_thread_data.push_notify_all();
     _load_flag = true;
 }
 
@@ -1431,6 +1432,9 @@ void PQFlashIndex<T, LabelT>::cached_beam_search(const T *query1, const uint64_t
         }
         compute_dists(&best_medoid, 1, dist_scratch);
         retset.insert(Neighbor(best_medoid, dist_scratch[0]));
+#ifdef DISKANN_DEBUG_PRINT_RETSET
+        stats->query_retset.push_back(Neighbor(best_medoid, dist_scratch[0]));
+#endif
         visited.insert(best_medoid);
         cur_list_size = 1;
     } else {
@@ -1638,6 +1642,10 @@ void PQFlashIndex<T, LabelT>::cached_beam_search(const T *query1, const uint64_t
                     float dist = dist_scratch[m];
                     Neighbor nn(id, dist);
                     retset.insert(nn);
+#ifdef DISKANN_DEBUG_PRINT_RETSET
+                    stats->query_retset.push_back(nn);
+#endif
+
                 }
             }
         }
@@ -1712,6 +1720,10 @@ void PQFlashIndex<T, LabelT>::cached_beam_search(const T *query1, const uint64_t
 
                     Neighbor nn(id, dist);
                     retset.insert(nn);
+#ifdef DISKANN_DEBUG_PRINT_RETSET
+                    stats->query_retset.push_back(nn);
+#endif
+
                 }
             }
 
@@ -1724,6 +1736,18 @@ void PQFlashIndex<T, LabelT>::cached_beam_search(const T *query1, const uint64_t
     }
     // re-sort by distance
     std::sort(full_retset.begin(), full_retset.end());
+
+#ifdef DISKANN_DEBUG_PRINT_RETSET
+    {
+        for (int i = 0; i < retset.size(); i++)
+        {
+            if (stats != nullptr)
+            {
+                stats->query_retset.push_back(retset[i]);
+            }
+        }
+    }
+#endif
 
     if (use_reorder_data)
     {
