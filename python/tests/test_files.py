ea2db1bd26ba7142e2a527379ea4338379f57812
Dax Pryce <daxpryce@microsoft.com>
2024-10-16 23:55:41 +0000

    This commit adds some basic memmap support to the `diskannpy.vectors_from_file` utility function. You can now return memory mapped np.array conformant view vs. requiring it beloaded fully into memory.

---
+++ python/tests/test_files.py
@@ -0,0 +1,39 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT license.
+
+import unittest
+
+import numpy as np
+
+from fixtures import random_vectors, vectors_as_temp_file
+
+import diskannpy as dap
+
+
+class TestVectorsFromFile(unittest.TestCase):
+    def test_in_mem(self):
+        expected = random_vectors(10_000, 100, dtype=np.float32)
+        with vectors_as_temp_file(expected) as vecs_file:
+            actual = dap.vectors_from_file(vecs_file, dtype=np.float32)
+            self.assertTrue(all(expected == actual))
+
+    def test_memmap(self):
+        expected = random_vectors(10_000, 100, dtype=np.float32)
+        with vectors_as_temp_file(expected) as vecs_file:
+            actual = dap.vectors_from_file(
+                vecs_file,
+                dtype=np.float32,
+                use_memmap=True
+            )
+            self.assertTrue(all(expected == actual))
+            actual = dap.vectors_from_file(
+                vecs_file,
+                dtype=np.float32,
+                use_memap=True,
+                mode="r+"
+            )
+            self.assertTrue(all(expected == actual))
+
+
+if __name__ == '__main__':
+    unittest.main()
