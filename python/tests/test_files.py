5db81defb46b8d0477550405870504935923c2fc
Dax Pryce <daxpryce@microsoft.com>
2024-10-23 23:44:25 +0000

    Windows does not appreciate two readers of the same file at the same time in our unit test.

--- python/tests/test_files.py
+++ python/tests/test_files.py
@@ -2,6 +2,8 @@
 # Licensed under the MIT license.
 
 import unittest
+import shutil
+import tempfile
 
 import numpy as np
 
@@ -15,25 +17,29 @@ class TestVectorsFromFile(unittest.TestCase):
         expected = random_vectors(10_000, 100, dtype=np.float32)
         with vectors_as_temp_file(expected) as vecs_file:
             actual = dap.vectors_from_file(vecs_file, dtype=np.float32)
-            self.assertTrue((expected == actual).all())
+            self.assertTrue((expected == actual).all(), f"{expected == actual}\n{expected}\n{actual}")
 
     def test_memmap(self):
         expected = random_vectors(10_000, 100, dtype=np.float32)
         with vectors_as_temp_file(expected) as vecs_file:
-            actual = dap.vectors_from_file(
-                vecs_file,
-                dtype=np.float32,
-                use_memmap=True
-            )
-            self.assertTrue((expected == actual).all(), f"{expected == actual}\n{expected}\n{actual}")
-            del actual # hopefully this releases the handle on the file, as windows is not happy with this
-            actual = dap.vectors_from_file(
-                vecs_file,
-                dtype=np.float32,
-                use_memmap=True,
-                mode="r+"
-            )
-            self.assertTrue((expected == actual).all())
+            with tempfile.NamedTemporaryFile() as vecs_file_copy:
+                shutil.copyfile(vecs_file, vecs_file_copy)
+                actual = dap.vectors_from_file(
+                    vecs_file,
+                    dtype=np.float32,
+                    use_memmap=True
+                )
+                self.assertTrue((expected == actual).all(), f"{expected == actual}\n{expected}\n{actual}")
+                # windows refuses to allow 2 active handles via memmap to touch the same file
+                # that's why we made a copy of the file itself and are using the copy here to test
+                # the read+append(inmem) 
+                actual = dap.vectors_from_file(
+                    vecs_file_copy,
+                    dtype=np.float32,
+                    use_memmap=True,
+                    mode="r+"
+                )
+                self.assertTrue((expected == actual).all(), f"{expected == actual}\n{expected}\n{actual}")
 
 
 if __name__ == '__main__':
