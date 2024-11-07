28f4ac4ce49c006cdec2992cdbc39706a69ed896
ravishankar <rakri@microsoft.com>
2024-11-07 05:12:46 -0800

    add baseline results

--- run-search.sh
+++ run-search.sh
@@ -1,2 +1,3 @@
 cd build
-./apps/search_memory_index --data_type int8 --dist_fn l2 --index_path_prefix /nvmessd1/fbv4/avarhade/prec40M_sorted_memory_index_r64_l100 --gt_file /nvmessd1/fbv4/gt100_prec40M --query_file /nvmessd1/fbv4/queries384d.bin  --result_path /home/rakri/avarhade/Dump/tmp -K 50 -L 50 60 70 80 90 100 -T 48 > /home/rakri/avarhade/DiskANN/prec40M/sorted_r64_k50.txt
+./apps/search_memory_index --data_type int8 --dist_fn l2 --index_path_prefix /nvmessd1/fbv4/avarhade/prec40M_sorted_memory_index_r64_l100 --gt_file /nvmessd1/fbv4/gt100_prec40M --query_file /nvmessd1/fbv4/queries384d.bin  --result_path /home/rakri/avarhade/Dump/tmp -K 50 -L 50 60 70 80 90 100 -T 1 > /home/rakri/avarhade/DiskANN/prec40M/sorted_r64_k50.txt
+
