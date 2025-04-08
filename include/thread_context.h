6c81054397df23ce63142f8095e58df31f9ac827
Li Tan <litan1@microsoft.com>
2025-04-08 16:05:29 -0700

    thread context for omp parallel to pass thread local allocator

---
+++ include/thread_context.h
@@ -0,0 +1,49 @@
+#ifndef _THREAD_CONTEXT_H_
+#define _THREAD_CONTEXT_H_
+
+void SetContextFuncs(void *getter, void *setter);
+
+class OmpParallelContext
+{
+public:
+    OmpParallelContext();
+    OmpParallelContext(const OmpParallelContext& other);
+    ~OmpParallelContext();
+
+private:
+    void* m_savedContext;
+};
+
+class DefaultThreadContext
+{
+public:
+    DefaultThreadContext();
+    ~DefaultThreadContext();
+
+    void* SavedContext();
+
+private:
+    void* m_savedContext;
+};
+
+class ThreadContext
+{
+public:
+    ThreadContext(void* context);
+    ~ThreadContext();
+
+private:
+    void* m_savedContext;
+};
+
+class SavePartitionContext
+{
+public:
+    SavePartitionContext();
+    ~SavePartitionContext();
+    void* SavedContext();
+
+private:
+    void* m_savedContext;
+};
+#endif
