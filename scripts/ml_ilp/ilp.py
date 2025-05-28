304c5bc4dfac824ff42ea560fb1312c7f766d931
Ananya Sutradhar <t-asutradhar@microsoft.com>
2025-05-28 12:56:13 +0000

    lp without slacks

--- scripts/ml_ilp/ilp.py
+++ scripts/ml_ilp/ilp.py
@@ -55,7 +55,7 @@ def direct_ratio_method(distances, matches, eps=1e-4):
     return w_d, w_m, total_pairs, 0
 
 
-def lp_soft_method(distances, matches, eps=1e-4, method ='lp'):
+def lp_soft_method_gekko(distances, matches, eps=1e-4):
     Q, N = distances.shape
     # Build LP
     if method == 'lp':
@@ -90,45 +90,74 @@ def lp_soft_method(distances, matches, eps=1e-4, method ='lp'):
         slack_vals = [float(s.value[0]) for s in slacks]
         violations = sum(1 for v in slack_vals if v > 1e-6)
         return float(w_d.value[0]), float(w_m.value[0]), len(slacks), violations
-    elif method == 'pulp':
-        print("using PuLP")
-        # Using PuLP
-        prob = pulp.LpProblem('VectorRanking', pulp.LpMinimize)
-        w_d = pulp.LpVariable('w_d', lowBound=1)
-        w_m = pulp.LpVariable('w_m', lowBound=0)
-        # prob += w_d + w_m == 2 #normalization constraint
+    
+def lp_soft_method_pulp(distances, matches, eps=1e-4):    
+    print("using PuLP")
+    # Using PuLP
+    prob = pulp.LpProblem('VectorRanking', pulp.LpMinimize)
+    w_d = pulp.LpVariable('w_d', lowBound=1)
+    w_m = pulp.LpVariable('w_m', lowBound=0)
+    # prob += w_d + w_m == 2 #normalization constraint
 
-        slacks = []
-        for q in tqdm(range(Q), desc="Building PuLP constraints"):
-            d = distances[q]
-            mvals = matches[q]
-            pos = np.where(mvals == 1)[0]
-            neg = np.where((mvals == 0) | (mvals == 0.5))[0]
-            for i in pos:
-                # neg_sample = np.random.choice(neg, size=min(10, len(neg)), replace=False)
-                for j in neg:
-                    if d[i] < d[j]:
-                        continue
-                    s = pulp.LpVariable(f's_{q}_{i}_{j}', lowBound=0)
-                    slacks.append(s)
-                    prob += (w_d * d[i] + w_m * (1 - mvals[i]) + eps
-                             <= w_d * d[j] + w_m * (1 - mvals[j]) + s)
-        print(f"Total equations: {len(slacks)}")
-        alpha = 500 # or any value you want
-        print(f"Using alpha = {alpha} for normalization")
+    slacks = []
+    for q in tqdm(range(Q), desc="Building PuLP constraints"):
+        d = distances[q]
+        mvals = matches[q]
+        pos = np.where(mvals == 1)[0]
+        neg = np.where((mvals == 0) | (mvals == 0.5))[0]
+        for i in pos:
+            # neg_sample = np.random.choice(neg, size=min(10, len(neg)), replace=False)
+            for j in neg:
+                if d[i] < d[j]:
+                    continue
+                s = pulp.LpVariable(f's_{q}_{i}_{j}', lowBound=0)
+                slacks.append(s)
+                prob += (w_d * d[i] + w_m * (1 - mvals[i]) + eps
+                            <= w_d * d[j] + w_m * (1 - mvals[j]) + s)
+    print(f"Total equations: {len(slacks)}")
+    alpha = 500 # or any value you want
+    print(f"Using alpha = {alpha} for normalization")
 
-        if len(slacks) > 0:
-            avg_slack = pulp.lpSum(slacks) / len(slacks)
-        else:
-            avg_slack = 0
+    if len(slacks) > 0:
+        avg_slack = pulp.lpSum(slacks) / len(slacks)
+    else:
+        avg_slack = 0
 
-        prob += w_m + alpha * avg_slack
-        # prob += pulp.lpSum(slacks)
-        print("Solving LP...")
-        prob.solve(pulp.PULP_CBC_CMD(msg=False))
-        slack_vals = [v.value() for v in slacks]
-        violations = sum(1 for v in slack_vals if v > 1e-6)
-        return w_d.value(), w_m.value(), len(slacks), violations
+    prob += w_m + alpha * avg_slack
+    # prob += pulp.lpSum(slacks)
+    print("Solving LP...")
+    prob.solve(pulp.PULP_CBC_CMD(msg=False))
+    slack_vals = [v.value() for v in slacks]
+    violations = sum(1 for v in slack_vals if v > 1e-6)
+    return w_d.value(), w_m.value(), len(slacks), violations
+    
+    
+def lp_soft_method_without_slack(distances, matches, eps=1e-4, method ='lp_wo_slack'): 
+    Q, N = distances.shape   
+    print("using PuLP without slacks")
+    # Using PuLP
+    prob = pulp.LpProblem('VectorRanking', pulp.LpMinimize)
+    w_m = pulp.LpVariable('w_m', lowBound=0)
+    num_equations = 0
+    # prob += w_d + w_m == 2 #normalization constraint
+
+    for q in tqdm(range(Q), desc="Building PuLP constraints"):
+        d = distances[q]
+        mvals = matches[q]
+        pos = np.where(mvals == 1)[0]
+        neg = np.where((mvals == 0) | (mvals == 0.5))[0]
+        for i in pos:
+            for j in neg:
+                if d[i] < d[j]:
+                    continue
+                prob += (d[i] + w_m * (1 - mvals[i]) + eps <= d[j] + w_m * (1 - mvals[j]))
+                num_equations += 1
+    prob += w_m 
+    print("Solving LP...")
+    print("eps:", eps)
+    prob.solve(pulp.PULP_CBC_CMD(msg=False))
+    return w_m.value(), num_equations
+    
 
 
 def main():
@@ -136,7 +165,7 @@ def main():
     parser.add_argument('unfiltered_ground_truth', help='Unfiltered Ground truth file (binary format)')
     parser.add_argument('filtered_ground_truth', help='Filtered Ground truth file (binary format)')
     parser.add_argument('filter_matches', help='Filter match file (binary match scores)')
-    parser.add_argument('--method', choices=['ratio', 'lp', 'pulp'], default='ratio')
+    parser.add_argument('--method', choices=['ratio', 'gekko', 'pulp', 'pulp_wo_slack'], default='ratio')
     parser.add_argument('--eps', type=float, default=1e-4)
     parser.add_argument('--plot', action='store_true')
     args = parser.parse_args()
@@ -176,12 +205,17 @@ def main():
     print(f"Distances: {distances[0][:5]}")
     # distances_scaled = distances / distances.max()
     # print(f"Scaled distances: {distances_scaled[0][:5]}")
+    w_d = 1.0  # Default weight for distances
+    violations = 0
 
     if args.method == 'ratio':
         w_d, w_m, total_pairs, _ = direct_ratio_method(distances, filter_matches, args.eps)
-        violations = 0
-    else:
-        w_d, w_m, total_pairs, violations = lp_soft_method(distances, filter_matches, args.eps, args.method)
+    if args.method == 'pulp_wo_slack':
+        w_m, total_pairs = lp_soft_method_without_slack(distances, filter_matches, args.eps)
+    if args.method == 'gekko':
+        w_d, w_m, total_pairs, violations = lp_soft_method_gekko(distances, filter_matches, args.eps)
+    if args.method == 'pulp':
+        w_d, w_m, total_pairs, violations = lp_soft_method_pulp(distances, filter_matches, args.eps)
 
     print(f"Method: {args.method}")
     print(f"w_d = {w_d:.6f}, w_m = {w_m:.6f}")
