04cfa04e33803d77013c544c5078a65ac325d515
Jordan Maples <jordanmaples@microsoft.com>
2026-04-20 13:38:41 -0700

    adding back integration test that exersizes the range_search against SIFT

--- diskann-providers/src/test_utils/mod.rs
+++ diskann-providers/src/test_utils/mod.rs
@@ -5,5 +5,5 @@
 
 mod search_utils;
 #[cfg(test)]
-pub use search_utils::is_match;
+pub use search_utils::{assert_range_results_exactly_match, is_match};
 pub use search_utils::{assert_top_k_exactly_match, groundtruth};
