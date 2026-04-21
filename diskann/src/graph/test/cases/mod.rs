2d93880d35688beafebda9fd4d9865d4ef5a5524
Jordan Maples <49793787+JordanMaples@users.noreply.github.com>
2026-04-21 12:43:24 -0700

    Apply suggestion from @JordanMaples

--- diskann/src/graph/test/cases/mod.rs
+++ diskann/src/graph/test/cases/mod.rs
@@ -7,8 +7,8 @@ mod consolidate;
 mod grid_insert;
 mod grid_search;
 mod inplace_delete;
-mod range_search;
 mod paged_search;
+mod range_search;
 
 /// Set to `true` and recompile to include full adjacency list state in participating
 /// baselines.
