9f81eaeaedd12e825487c29e9f0ec430eecff3e3
Jordan Maples <jordanmaples@microsoft.com>
2026-04-28 10:21:38 -0700

    use macro instead

--- diskann-quantization/src/num.rs
+++ diskann-quantization/src/num.rs
@@ -60,159 +60,94 @@ pub struct PowerOfTwo(NonZeroUsize);
 #[non_exhaustive]
 pub struct NotPowerOfTwo(usize);
 
-/// PowerOfTwo constants for all valid exponents (0..Usize::BITS-1) for 32 bit architectures
-///
-/// # Safety
-///
-/// 1 << N is a power of two for all N in 0..Usize::BITS-1.
-#[allow(clippy::undocumented_unsafe_blocks)]
-impl PowerOfTwo {
-    pub const P0: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 0).unwrap()) };
-    pub const P1: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 1).unwrap()) };
-    pub const P2: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 2).unwrap()) };
-    pub const P3: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 3).unwrap()) };
-    pub const P4: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 4).unwrap()) };
-    pub const P5: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 5).unwrap()) };
-    pub const P6: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 6).unwrap()) };
-    pub const P7: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 7).unwrap()) };
-    pub const P8: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 8).unwrap()) };
-    pub const P9: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 9).unwrap()) };
-    pub const P10: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 10).unwrap()) };
-    pub const P11: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 11).unwrap()) };
-    pub const P12: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 12).unwrap()) };
-    pub const P13: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 13).unwrap()) };
-    pub const P14: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 14).unwrap()) };
-    pub const P15: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 15).unwrap()) };
-    pub const P16: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 16).unwrap()) };
-    pub const P17: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 17).unwrap()) };
-    pub const P18: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 18).unwrap()) };
-    pub const P19: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 19).unwrap()) };
-    pub const P20: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 20).unwrap()) };
-    pub const P21: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 21).unwrap()) };
-    pub const P22: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 22).unwrap()) };
-    pub const P23: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 23).unwrap()) };
-    pub const P24: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 24).unwrap()) };
-    pub const P25: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 25).unwrap()) };
-    pub const P26: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 26).unwrap()) };
-    pub const P27: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 27).unwrap()) };
-    pub const P28: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 28).unwrap()) };
-    pub const P29: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 29).unwrap()) };
-    pub const P30: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 30).unwrap()) };
-    pub const P31: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 31).unwrap()) };
-}
+macro_rules! constants {
+    (($pow:ident, $value:ident) => $shift:literal) => {
+        pub const $pow: Self = match Self::new(1 << $shift) {
+            Ok(v) => v,
+            Err(_) => panic!("not a power of two"),
+        };
 
-/// PowerOfTwo constants for all valid exponents (32..Usize::BITS-1) for 64 bit architectures
-///
-/// # Safety
-///
-/// 1 << N is a power of two for all N in 0..Usize::BITS-1.
-#[cfg(target_pointer_width = "64")]
-#[allow(clippy::undocumented_unsafe_blocks)]
-impl PowerOfTwo {
-    pub const P32: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 32).unwrap()) };
-    pub const P33: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 33).unwrap()) };
-    pub const P34: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 34).unwrap()) };
-    pub const P35: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 35).unwrap()) };
-    pub const P36: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 36).unwrap()) };
-    pub const P37: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 37).unwrap()) };
-    pub const P38: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 38).unwrap()) };
-    pub const P39: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 39).unwrap()) };
-    pub const P40: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 40).unwrap()) };
-    pub const P41: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 41).unwrap()) };
-    pub const P42: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 42).unwrap()) };
-    pub const P43: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 43).unwrap()) };
-    pub const P44: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 44).unwrap()) };
-    pub const P45: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 45).unwrap()) };
-    pub const P46: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 46).unwrap()) };
-    pub const P47: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 47).unwrap()) };
-    pub const P48: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 48).unwrap()) };
-    pub const P49: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 49).unwrap()) };
-    pub const P50: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 50).unwrap()) };
-    pub const P51: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 51).unwrap()) };
-    pub const P52: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 52).unwrap()) };
-    pub const P53: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 53).unwrap()) };
-    pub const P54: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 54).unwrap()) };
-    pub const P55: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 55).unwrap()) };
-    pub const P56: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 56).unwrap()) };
-    pub const P57: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 57).unwrap()) };
-    pub const P58: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 58).unwrap()) };
-    pub const P59: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 59).unwrap()) };
-    pub const P60: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 60).unwrap()) };
-    pub const P61: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 61).unwrap()) };
-    pub const P62: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 62).unwrap()) };
-    pub const P63: Self = unsafe { Self::new_unchecked(NonZeroUsize::new(1 << 63).unwrap()) };
+        pub const $value: Self = Self::$pow;
+    };
+    ($(($pow:ident, $value:ident) => $shift:literal),+ $(,)?) => {
+        $(constants!(($pow, $value) => $shift);)+
+    };
 }
 
 impl PowerOfTwo {
-    /// V values for some of the equivalent P(owersOfTwo)'s
-    pub const V1: Self = Self::P0;
-    pub const V2: Self = Self::P1;
-    pub const V4: Self = Self::P2;
-    pub const V8: Self = Self::P3;
-    pub const V16: Self = Self::P4;
-    pub const V32: Self = Self::P5;
-    pub const V64: Self = Self::P6;
-    pub const V128: Self = Self::P7;
-    pub const V256: Self = Self::P8;
-    pub const V512: Self = Self::P9;
-    pub const V1024: Self = Self::P10;
-    pub const V2048: Self = Self::P11;
-    pub const V4096: Self = Self::P12;
-    pub const V8192: Self = Self::P13;
-    pub const V16384: Self = Self::P14;
-    pub const V32768: Self = Self::P15;
-    pub const V65536: Self = Self::P16;
-    pub const V131072: Self = Self::P17;
-    pub const V262144: Self = Self::P18;
-    pub const V524288: Self = Self::P19;
-    pub const V1048576: Self = Self::P20;
-    pub const V2097152: Self = Self::P21;
-    pub const V4194304: Self = Self::P22;
-    pub const V8388608: Self = Self::P23;
-    pub const V16777216: Self = Self::P24;
-    pub const V33554432: Self = Self::P25;
-    pub const V67108864: Self = Self::P26;
-    pub const V134217728: Self = Self::P27;
-    pub const V268435456: Self = Self::P28;
-    pub const V536870912: Self = Self::P29;
-    pub const V1073741824: Self = Self::P30;
-    pub const V2147483648: Self = Self::P31;
+    constants! {
+        (P0, V1) => 0,
+        (P1, V2) => 1,
+        (P2, V4) => 2,
+        (P3, V8) => 3,
+        (P4, V16) => 4,
+        (P5, V32) => 5,
+        (P6, V64) => 6,
+        (P7, V128) => 7,
+        (P8, V256) => 8,
+        (P9, V512) => 9,
+        (P10, V1024) => 10,
+        (P11, V2048) => 11,
+        (P12, V4096) => 12,
+        (P13, V8192) => 13,
+        (P14, V16384) => 14,
+        (P15, V32768) => 15,
+        (P16, V65536) => 16,
+        (P17, V131072) => 17,
+        (P18, V262144) => 18,
+        (P19, V524288) => 19,
+        (P20, V1048576) => 20,
+        (P21, V2097152) => 21,
+        (P22, V4194304) => 22,
+        (P23, V8388608) => 23,
+        (P24, V16777216) => 24,
+        (P25, V33554432) => 25,
+        (P26, V67108864) => 26,
+        (P27, V134217728) => 27,
+        (P28, V268435456) => 28,
+        (P29, V536870912) => 29,
+        (P30, V1073741824) => 30,
+        (P31, V2147483648) => 31,
+    }
 }
 
 #[cfg(target_pointer_width = "64")]
+#[allow(clippy::undocumented_unsafe_blocks)]
 impl PowerOfTwo {
-    pub const V4294967296: Self = Self::P32;
-    pub const V8589934592: Self = Self::P33;
-    pub const V17179869184: Self = Self::P34;
-    pub const V34359738368: Self = Self::P35;
-    pub const V68719476736: Self = Self::P36;
-    pub const V137438953472: Self = Self::P37;
-    pub const V274877906944: Self = Self::P38;
-    pub const V549755813888: Self = Self::P39;
-    pub const V1099511627776: Self = Self::P40;
-    pub const V2199023255552: Self = Self::P41;
-    pub const V4398046511104: Self = Self::P42;
-    pub const V8796093022208: Self = Self::P43;
-    pub const V17592186044416: Self = Self::P44;
-    pub const V35184372088832: Self = Self::P45;
-    pub const V70368744177664: Self = Self::P46;
-    pub const V140737488355328: Self = Self::P47;
-    pub const V281474976710656: Self = Self::P48;
-    pub const V562949953421312: Self = Self::P49;
-    pub const V1125899906842624: Self = Self::P50;
-    pub const V2251799813685248: Self = Self::P51;
-    pub const V4503599627370496: Self = Self::P52;
-    pub const V9007199254740992: Self = Self::P53;
-    pub const V18014398509481984: Self = Self::P54;
-    pub const V36028797018963968: Self = Self::P55;
-    pub const V72057594037927936: Self = Self::P56;
-    pub const V144115188075855872: Self = Self::P57;
-    pub const V288230376151711744: Self = Self::P58;
-    pub const V576460752303423488: Self = Self::P59;
-    pub const V1152921504606846976: Self = Self::P60;
-    pub const V2305843009213693952: Self = Self::P61;
-    pub const V4611686018427387904: Self = Self::P62;
-    pub const V9223372036854775808: Self = Self::P63;
+    constants! {
+        (P32, V4294967296) => 32,
+        (P33, V8589934592) => 33,
+        (P34, V17179869184) => 34,
+        (P35, V34359738368) => 35,
+        (P36, V68719476736) => 36,
+        (P37, V137438953472) => 37,
+        (P38, V274877906944) => 38,
+        (P39, V549755813888) => 39,
+        (P40, V1099511627776) => 40,
+        (P41, V2199023255552) => 41,
+        (P42, V4398046511104) => 42,
+        (P43, V8796093022208) => 43,
+        (P44, V17592186044416) => 44,
+        (P45, V35184372088832) => 45,
+        (P46, V70368744177664) => 46,
+        (P47, V140737488355328) => 47,
+        (P48, V281474976710656) => 48,
+        (P49, V562949953421312) => 49,
+        (P50, V1125899906842624) => 50,
+        (P51, V2251799813685248) => 51,
+        (P52, V4503599627370496) => 52,
+        (P53, V9007199254740992) => 53,
+        (P54, V18014398509481984) => 54,
+        (P55, V36028797018963968) => 55,
+        (P56, V72057594037927936) => 56,
+        (P57, V144115188075855872) => 57,
+        (P58, V288230376151711744) => 58,
+        (P59, V576460752303423488) => 59,
+        (P60, V1152921504606846976) => 60,
+        (P61, V2305843009213693952) => 61,
+        (P62, V4611686018427387904) => 62,
+        (P63, V9223372036854775808) => 63,
+    }
 }
 
 impl PowerOfTwo {
