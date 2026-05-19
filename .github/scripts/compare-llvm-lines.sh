f9296ded34257dab9d81efa3e50cf998bb64e5f8
Alex Razumov (from Dev Box) <alrazu@microsoft.com>
2026-05-19 16:02:53 -0700

    Delta and Current columns sort numerically

--- .github/scripts/compare-llvm-lines.sh
+++ .github/scripts/compare-llvm-lines.sh
@@ -52,6 +52,6 @@ printf "| %+d | %s | %s | (TOTAL) |\n" "$delta_total" "$current_total" "$baselin
 join -t$'\t' -j 2 -a1 -a2 -e 0 -o 0,1.1,2.1 \
   "$current_parsed_file" "$baseline_parsed_file" | \
   awk -F'\t' '{delta=$2-$3; printf "%d\t%s\t%s\t%s\n", delta, $2, $3, $1}' | \
-  sort -t$'\t' -rn | \
+  sort -t$'\t' -k1,1rn -k2,2rn | \
   awk -F'\t' '{printf "| %+d | %s | %s | %s |\n", $1, $2, $3, $4}'
 echo ""
