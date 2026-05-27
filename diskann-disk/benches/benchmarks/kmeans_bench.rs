8b9bd93706d2e67c493242e5289a7d2eff33dbe1
Alex Razumov (from Dev Box) <alrazu@microsoft.com>
2026-05-26 19:10:58 -0700

    Removing comparison

--- diskann-disk/benches/benchmarks/kmeans_bench.rs
+++ diskann-disk/benches/benchmarks/kmeans_bench.rs
@@ -4,63 +4,14 @@
  */
 
 use criterion::{BatchSize, Criterion};
-use diskann::{ANNError, ANNResult};
 use diskann_disk::utils::{compute_vecs_l2sq, k_means_clustering};
-use diskann_providers::utils::{create_thread_pool_for_bench, RayonThreadPoolRef};
+use diskann_providers::utils::create_thread_pool_for_bench;
 use rand::Rng;
-use rayon::prelude::*;
-
-use diskann_providers::utils::ParallelIteratorInPool;
 
 const NUM_POINTS: usize = 100000;
 const DIM: usize = 4;
 const NUM_CENTERS: usize = 256;
 const MAX_KMEANS_REPS: usize = 12;
-
-/// Original scalar implementation for comparison.
-fn compute_vecs_l2sq_scalar(
-    vecs_l2sq: &mut [f32],
-    data: &[f32],
-    dim: usize,
-    pool: RayonThreadPoolRef<'_>,
-) -> ANNResult<()> {
-    if dim == 0 {
-        return Err(ANNError::log_index_error(format_args!(
-            "dim must be non-zero"
-        )));
-    }
-
-    let expected_data_len = vecs_l2sq.len().checked_mul(dim).ok_or_else(|| {
-        ANNError::log_index_error(format_args!(
-            "vecs_l2sq.len() * dim overflowed: vecs_l2sq.len() ({}) * dim ({})",
-            vecs_l2sq.len(),
-            dim
-        ))
-    })?;
-
-    if data.len() != expected_data_len {
-        return Err(ANNError::log_index_error(format_args!(
-            "data.len() ({}) should be vecs_l2sq.len() ({}) * dim ({})",
-            data.len(),
-            vecs_l2sq.len(),
-            dim
-        )));
-    }
-
-    if dim < 5 {
-        for (vec_l2sq, chunk) in vecs_l2sq.iter_mut().zip(data.chunks_exact(dim)) {
-            *vec_l2sq = chunk.iter().map(|v| v * v).sum();
-        }
-    } else {
-        vecs_l2sq
-            .par_iter_mut()
-            .zip(data.par_chunks_exact(dim))
-            .for_each_in_pool(pool, |(vec_l2sq, chunk)| {
-                *vec_l2sq = chunk.iter().map(|v| v * v).sum();
-            });
-    }
-    Ok(())
-}
 
 pub fn benchmark_kmeans(c: &mut Criterion) {
     let rng = &mut diskann_providers::utils::create_rnd_from_seed(42);
@@ -88,16 +39,6 @@ pub fn benchmark_kmeans(c: &mut Criterion) {
                     &mut false,
                     pool.as_ref(),
                 )
-            },
-            BatchSize::SmallInput,
-        )
-    });
-
-    group.bench_function("Snrm2 Scalar", |f| {
-        f.iter_batched(
-            || (data.clone(), vec![0.0f32; NUM_POINTS]),
-            |(data_copy, mut docs_l2sq)| {
-                compute_vecs_l2sq_scalar(&mut docs_l2sq, &data_copy, DIM, pool.as_ref()).unwrap();
             },
             BatchSize::SmallInput,
         )
--- diskann-disk/benches/benchmarks_iai/kmeans_bench_iai.rs
+++ diskann-disk/benches/benchmarks_iai/kmeans_bench_iai.rs
@@ -5,16 +5,9 @@
 
 use std::hint::black_box;
 
-use diskann::{ANNError, ANNResult};
 use diskann_disk::utils::{compute_vecs_l2sq, k_means_clustering};
-use diskann_providers::utils::{
-    create_thread_pool_for_bench, ParallelIteratorInPool, RayonThreadPool, RayonThreadPoolRef,
-};
+use diskann_providers::utils::{create_thread_pool_for_bench, RayonThreadPool};
 use rand::Rng;
-use rayon::{
-    iter::{IndexedParallelIterator, IntoParallelRefMutIterator},
-    slice::ParallelSlice,
-};
 
 const NUM_POINTS: usize = 100000;
 const DIM: usize = 4;
@@ -23,53 +16,8 @@ const MAX_KMEANS_REPS: usize = 12;
 
 iai_callgrind::library_benchmark_group!(
     name = kmeans_bench_iai;
-    benchmarks = benchmark_kmeans_iai, snrm2_benchmark_rust_iai, snrm2_benchmark_scalar_iai,
+    benchmarks = benchmark_kmeans_iai, snrm2_benchmark_rust_iai,
 );
-
-/// Original scalar implementation for comparison.
-fn compute_vecs_l2sq_scalar(
-    vecs_l2sq: &mut [f32],
-    data: &[f32],
-    dim: usize,
-    pool: RayonThreadPoolRef<'_>,
-) -> ANNResult<()> {
-    if dim == 0 {
-        return Err(ANNError::log_index_error(format_args!(
-            "dim must be non-zero"
-        )));
-    }
-
-    let expected_data_len = vecs_l2sq.len().checked_mul(dim).ok_or_else(|| {
-        ANNError::log_index_error(format_args!(
-            "vecs_l2sq.len() * dim overflowed: vecs_l2sq.len() ({}) * dim ({})",
-            vecs_l2sq.len(),
-            dim
-        ))
-    })?;
-
-    if data.len() != expected_data_len {
-        return Err(ANNError::log_index_error(format_args!(
-            "data.len() ({}) should be vecs_l2sq.len() ({}) * dim ({})",
-            data.len(),
-            vecs_l2sq.len(),
-            dim
-        )));
-    }
-
-    if dim < 5 {
-        for (vec_l2sq, chunk) in vecs_l2sq.iter_mut().zip(data.chunks_exact(dim)) {
-            *vec_l2sq = chunk.iter().map(|v| v * v).sum();
-        }
-    } else {
-        vecs_l2sq
-            .par_iter_mut()
-            .zip(data.par_chunks_exact(dim))
-            .for_each_in_pool(pool, |(vec_l2sq, chunk)| {
-                *vec_l2sq = chunk.iter().map(|v| v * v).sum();
-            });
-    }
-    Ok(())
-}
 
 fn setup_data() -> (Vec<f32>, RayonThreadPool) {
     let rng = &mut diskann_providers::utils::create_rnd_from_seed(42);
@@ -106,12 +54,5 @@ pub fn benchmark_kmeans_iai((data, pool): (Vec<f32>, RayonThreadPool)) {
 pub fn snrm2_benchmark_rust_iai((data, pool): (Vec<f32>, RayonThreadPool)) {
     let mut docs_l2sq = vec![0.0; NUM_POINTS];
     compute_vecs_l2sq(&mut docs_l2sq, black_box(&data), DIM, pool.as_ref()).unwrap();
-    black_box(docs_l2sq);
-}
-
-#[iai_callgrind::library_benchmark(setup = setup_data)]
-pub fn snrm2_benchmark_scalar_iai((data, pool): (Vec<f32>, RayonThreadPool)) {
-    let mut docs_l2sq = vec![0.0; NUM_POINTS];
-    compute_vecs_l2sq_scalar(&mut docs_l2sq, black_box(&data), DIM, pool.as_ref()).unwrap();
     black_box(docs_l2sq);
 }
