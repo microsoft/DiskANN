9c1d1128437f9456aef790425e6fac4f276d4996
Magdalen Manohar <mmanohar@microsoft.com>
2026-06-04 15:58:35 +0000

    add test in diskann-benchmark-core for inline search

--- diskann-benchmark-core/src/search/graph/inline.rs
+++ diskann-benchmark-core/src/search/graph/inline.rs
@@ -136,3 +136,175 @@ where
         })
     }
 }
+
+
+///////////
+// Tests //
+///////////
+
+#[cfg(test)]
+mod tests {
+    use std::num::NonZeroUsize;
+
+    use super::*;
+
+    use crate::recall::GroundTruthMode;
+    use diskann::graph::{index::QueryLabelProvider, test::provider};
+
+    // A simple [`QueryLabelProvider`] that rejects odd indices.
+    #[derive(Debug)]
+    struct NoOdds;
+
+    impl graph::index::QueryLabelProvider<u32> for NoOdds {
+        fn is_match(&self, id: u32) -> bool {
+            id.is_multiple_of(2)
+        }
+    }
+
+    #[test]
+    fn test_inline() {
+        let nearest_neighbors = 5;
+
+        let index = search::graph::test_grid_provider();
+
+        let mut queries = Matrix::new(0.0f32, 5, index.provider().dim());
+        queries.row_mut(0).copy_from_slice(&[0.0, 0.0, 0.0, 0.0]);
+        queries.row_mut(1).copy_from_slice(&[4.0, 0.0, 0.0, 0.0]);
+        queries.row_mut(2).copy_from_slice(&[0.0, 4.0, 0.0, 0.0]);
+        queries.row_mut(3).copy_from_slice(&[0.0, 0.0, 4.0, 0.0]);
+        queries.row_mut(4).copy_from_slice(&[0.0, 0.0, 0.0, 4.0]);
+
+        let queries = Arc::new(queries);
+
+        let adaptive_l = graph::search::AdaptiveL::new(10, 16.0).unwrap();
+
+        let inline = InlineSearch::new(
+            index,
+            queries.clone(),
+            Strategy::broadcast(provider::Strategy::new()),
+            (0..queries.nrows())
+                .map(|_| -> Arc<dyn QueryLabelProvider<_>> { Arc::new(NoOdds {}) })
+                .collect(),
+            Some(adaptive_l),
+        )
+        .unwrap();
+
+        // Test the standard search interface.
+        let rt = crate::tokio::runtime(2).unwrap();
+        let results = search::search(
+            inline.clone(),
+            graph::search::Knn::new(nearest_neighbors, 10, None).unwrap(),
+            NonZeroUsize::new(2).unwrap(),
+            &rt,
+        )
+        .unwrap();
+
+        assert_eq!(results.len(), queries.nrows());
+        let rows = results.ids().as_rows();
+        assert_eq!(*rows.row(0).first().unwrap(), 0);
+
+        // Check that only even IDs are returned.
+        for r in 0..rows.nrows() {
+            assert_eq!(rows.row(r).len(), nearest_neighbors);
+            for &id in rows.row(r) {
+                assert_eq!(id % 2, 0, "Found odd ID {} in row {}", id, r);
+            }
+        }
+
+        const TWO: NonZeroUsize = NonZeroUsize::new(2).unwrap();
+        let setup = search::Setup {
+            threads: TWO,
+            tasks: TWO,
+            reps: TWO,
+        };
+
+        // Try the aggregated strategy.
+        let parameters = [
+            search::Run::new(
+                graph::search::Knn::new(nearest_neighbors, 10, None).unwrap(),
+                setup.clone(),
+            ),
+            search::Run::new(
+                graph::search::Knn::new(nearest_neighbors, 15, None).unwrap(),
+                setup.clone(),
+            ),
+        ];
+
+        let recall_k = nearest_neighbors;
+        let recall_n = nearest_neighbors;
+
+        let all = search::search_all(
+            inline,
+            parameters,
+            search::graph::knn::Aggregator::new(
+                rows,
+                recall_k,
+                recall_n,
+                GroundTruthMode::Flexible,
+            ),
+        )
+        .unwrap();
+
+        assert_eq!(all.len(), 2);
+        for summary in all {
+            assert_eq!(summary.setup, setup);
+            assert_eq!(summary.end_to_end_latencies.len(), TWO.get());
+            assert_eq!(summary.mean_latencies.len(), TWO.get());
+            assert_eq!(summary.p90_latencies.len(), TWO.get());
+            assert_eq!(summary.p99_latencies.len(), TWO.get());
+
+            assert_ne!(summary.mean_cmps, 0.0);
+            assert_ne!(summary.mean_hops, 0.0);
+
+            let recall = summary.recall;
+            assert_eq!(recall.recall_k, recall_k);
+            assert_eq!(recall.recall_n, recall_n);
+            assert_eq!(recall.num_queries, queries.nrows());
+            assert_eq!(recall.average, 1.0, "we used a search as the groundtruth");
+        }
+    }
+
+    #[test]
+    fn test_inline_error() {
+        let index = search::graph::test_grid_provider();
+        let queries = Arc::new(Matrix::new(0.0f32, 2, index.provider().dim()));
+
+        let labels: Arc<[_]> = (0..queries.nrows() + 1)
+            .map(|_| -> Arc<dyn QueryLabelProvider<_>> { Arc::new(NoOdds {}) })
+            .collect();
+
+        let strategy = provider::Strategy::new();
+
+        // Error for a mismatch between strategies and queries.
+        let err = InlineSearch::new(
+            index.clone(),
+            queries.clone(),
+            Strategy::collection([strategy.clone()]),
+            labels.clone(),
+            None,
+        )
+        .unwrap_err();
+        let msg = err.to_string();
+        assert!(
+            msg.contains("1 strategy was provided when 2 were expected"),
+            "failed with {msg}"
+        );
+
+        // Error for a mismatch between label providers and queries.
+        let err = InlineSearch::new(
+            index,
+            queries.clone(),
+            Strategy::broadcast(strategy.clone()),
+            labels.clone(),
+            None,
+        )
+        .unwrap_err();
+        let msg = err.to_string();
+        assert!(
+            msg.contains(
+                "Number of label providers (3) must be equal to the number of queries (2)"
+            ),
+            "failed with {msg}"
+        );
+    }
+}
