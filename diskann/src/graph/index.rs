18d7b87dd983ea70c3d249f16f4b7920c4b74c4f
Mark Hildebrand <hildebrandmw@gmail.com>
2026-06-05 09:29:19 -0700

    Remove `try_into_vector_id` conversion in prune. (#1133)
    Pruning did kind of a sneaky trick of reusing the output neighbor buffer
    as temporary scratch space for indices during prune. While this worked,
    it relied on `usize` variables being convertible to `VectorId`, which is
    kind of dubious because (1) we are moving away from `VectorId`s being
    convertible to primitive values and (2) it doesn't really provide a good
    guarantee on **when** the conversion will succeed or not.
    
    This PR fixes that by using a dedicated scratch to store these indices.
    
    While I originally introduced a third `Vec` in `prune::{Scratch,
    Context}`, I realized that all the auxiliary variables could be
    collapsed into a single `State`. Note that we get the extra 2-bytes for
    `neighbor` basically for free because even without it, padding would
    bring the size of `State` to 8 bytes.
    
    Also, this fixes a potential bug where the max occlusion size could be
    set larger than `u16::MAX` and thus silently truncate via `_ as u16` by
    enforcing a `u16::MAX` bound in `diskann::graph::Config`. This far
    exceeds any practical value anyone should be setting for this value so
    is unlikely to result in any breakage.

--- diskann/src/graph/index.rs
+++ diskann/src/graph/index.rs
@@ -321,9 +321,8 @@ where
                         &mut search_record.visited,
                         self.max_occlusion_size(),
                     ),
-                    occlude_factor: &mut prune_scratch.occlude_factor,
+                    states: &mut prune_scratch.states,
                     neighbors: &mut new_neighbors,
-                    last_checked: &mut prune_scratch.last_checked,
                 };
 
                 let options = prune::Options {
@@ -2603,8 +2602,7 @@ where
 
             let mut context = prune::Context {
                 pool: SortedNeighbors::new(&mut record.visited, self.max_occlusion_size()),
-                occlude_factor: &mut scratch.occlude_factor,
-                last_checked: &mut scratch.last_checked,
+                states: &mut scratch.states,
                 neighbors: &mut scratch.neighbors,
             };
 
@@ -2663,28 +2661,27 @@ where
         C: for<'a, 'b> DistanceFunction<M::ElementRef<'a>, M::ElementRef<'b>, f32>,
         F: Fn(DP::InternalId) -> bool,
     {
-        if context.pool.is_empty() {
-            return;
-        }
+        assert!(
+            context.pool.len() <= u16::MAX.into_usize(),
+            "this has an upper bound set by `diskann::graph::Config` and should not exceed `u16::MAX`"
+        );
 
         let prune::Context {
             pool,
-            occlude_factor,
-            neighbors: dst,
-            last_checked,
+            states,
+            neighbors,
         } = context;
 
-        dst.clear();
-        occlude_factor.clear();
+        if pool.is_empty() {
+            neighbors.clear();
+            return;
+        }
 
         let alpha = self.config.alpha();
         let degree = self.config.pruned_degree().get();
 
-        occlude_factor.clear();
-        occlude_factor.resize(pool.len(), 0.0);
-
-        last_checked.clear();
-        last_checked.resize(pool.len(), 0u16);
+        states.clear();
+        states.resize(pool.len(), prune::State::default());
 
         let mut current_alpha = 1.0f32;
         let increment_factor = alpha.min(1.2);
@@ -2707,17 +2704,57 @@ where
             })
             .collect();
 
-        // Loop will also terminate after cur_alpha reaches alpha
-        while dst.len() < degree {
+        // For an alpha value `A`, a candidate `i` is promoted to a neighbor if for all
+        // ```
+        // max{j < i | j is a neighbor}(occlude_factor(i, j))
+        // ```
+        // This process happens with multiple values of `A`.
+        //
+        // We can compute this efficiently using the following rules:
+        //
+        // 1. For a candidate `i`, start scanning `j < i`, computing occlude factors.
+        // 2. If we find an occlude factor greater than `A`, record that `i` has visited
+        //    `j`, stop computing occlude factors, and move on to `i + 1`.
+        // 3. If we reach `j == i - 1` with the maximum occlude factor less than `A`, then
+        //    `i` gets promoted to a neighbor.
+        //
+        // On the implementation side, we use `states` in the following way:
+        //
+        // * `states[n].neighbor` is the **index** in `pool` of the `n`th **neighbor**.
+        //   Note that a "neighbor" is a candidate that passes pruning.
+        //
+        //   Very important: to get the index `j` in the above description, we need to
+        //   check `pool[states[n].neighbor]`.
+        //
+        //   This indexing naturally skips candidates `j` that have not been promoted to
+        //   neighbors.
+        //
+        // * `states[i].occlude_factor` is the maximum occlude factor found for a candidate
+        //   `i`. This gets set to `f32::MAX` when `i` is promoted to a neighbor which
+        //   excludes it from future consideration.
+        //
+        // * `states[i].last_checked` is the highest value of `n` against which the
+        //   occlude factor for `j = pool[states[n].neighbor]` has been checked.
+        //
+        //   The maximum value this should reach is `i`.
+        //
+        // Note that we use `states` for both "candidate" and "neighbor" tracking.
+        let mut found = 0;
+        while found < degree {
             for (i, (neighbor_distance, neighbor)) in cache.iter().enumerate() {
-                if dst.len() >= degree {
+                if found >= degree {
                     break;
                 }
 
-                let factor = &mut occlude_factor[i];
+                // The tracking states for candidate `i`.
+                let prune::State {
+                    mut occlude_factor,
+                    mut last_checked,
+                    ..
+                } = states[i];
 
                 // If the occlusion factor for this neighbor is too high, skip it.
-                if *factor > current_alpha {
+                if occlude_factor > current_alpha {
                     continue;
                 }
 
@@ -2727,39 +2764,28 @@ where
                 let neighbor = match neighbor {
                     Some(n) => n,
                     None => {
-                        *factor = f32::MAX;
+                        debug_assert!(states.get(i).is_some(), "index {i} is out of bounds");
+                        // SAFETY: We've already checked `states[i]`.
+                        unsafe { states.get_unchecked_mut(i) }.occlude_factor = f32::MAX;
                         continue;
                     }
                 };
-
-                // This neighbor has not been exluded.
-                //
-                // To determine whether or not to add it, we must compute its occlusion
-                // factor against all elements in `result` that appear before it in `pool`.
-                //
-                // This computation may be resumed from previous `alpha` values, so we need
-                // to access our scratch data structures.
-                let position = &mut last_checked[i];
-
-                // During computation, we've found an occlusion factor greater than alpha
-                // and wish to abort this neighbor from consideration.
-                let mut skip: bool = false;
 
                 // Increment `position` until we've compared with all current entries in
                 // `result`.
                 //
                 // When the list is empty, the loop is skipped allowing the first undeleted
                 // element to be added.
-                while *position as usize != dst.len() {
-                    let result_to_check = *position;
-                    // Increment the position pointer.
-                    *position += 1;
-
-                    let result_position = dst[result_to_check as usize].into_usize();
+                while last_checked as usize != found {
+                    let result_position = states[last_checked as usize].neighbor.into_usize();
+                    last_checked += 1;
 
                     // If the position of this result in `pool` is greater than or equal
                     // the current working position, then skip this candidate.
                     if result_position >= i {
+                        debug_assert!(states.get(i).is_some(), "index {i} is out of bounds");
+                        // SAFETY: We've already checked `states[i]`.
+                        unsafe { states.get_unchecked_mut(i) }.last_checked = last_checked;
                         continue;
                     }
 
@@ -2773,43 +2799,36 @@ where
                     };
 
                     // Update occlude factor
-                    *factor = self.config.prune_kind().update_occlude_factor(
+                    occlude_factor = self.config.prune_kind().update_occlude_factor(
                         *neighbor_distance,
                         distance,
-                        *factor,
+                        occlude_factor,
                         current_alpha,
                     );
 
                     // Check if the most recent update to the occlusion factor removes this
                     // neighbor from consideration.
-                    if *factor > current_alpha {
-                        // Don't add this neighbor.
-                        skip = true;
+                    if occlude_factor > current_alpha {
                         break;
                     }
                 }
 
-                // N.B.: We can get here straight from the self-neighbor check when the
-                // neighbor and the data at `location` have the same encoding.
-                //
-                // SO, we use short-circuiting logic to avoid checking the occlusion factor
-                // twice, though that might not really save much.
-                if skip || *factor > current_alpha {
+                debug_assert!(states.get(i).is_some(), "index {i} is out of bounds");
+                // SAFETY: We've already checked `states[i]`.
+                let state = unsafe { states.get_unchecked_mut(i) };
+
+                state.last_checked = last_checked;
+                if occlude_factor > current_alpha {
+                    state.occlude_factor = occlude_factor;
                     continue;
                 }
 
                 // This neighbor has passed all the requirements of being a candidate.
-                *factor = f32::MAX;
+                state.occlude_factor = f32::MAX;
 
                 // This conversion should always succeed.
-                #[expect(
-                    clippy::expect_used,
-                    reason = "`i` cannot exceed `u16::MAX` so conversion should succeed"
-                )]
-                dst.push(
-                    i.try_into_vector_id()
-                        .expect("argument should not exceed u16::MAX"),
-                );
+                states[found].neighbor = i as u16;
+                found += 1;
             }
 
             // Exit if we completed the final iteration.
@@ -2820,21 +2839,25 @@ where
             current_alpha = (current_alpha * increment_factor).min(alpha);
         }
 
-        // Cleanup `result` by undoing the local indirection.
-        dst.remap_trusted(|r| *r = pool[r.into_usize()].id);
-        debug_assert!(dst.len() <= degree, "max degree bound violated");
+        let mut guard = neighbors.resize(found);
+        std::iter::zip(guard.iter_mut(), states.iter()).for_each(|(d, s)| {
+            *d = pool[s.neighbor.into_usize()].id;
+        });
+        guard.finish(found);
+
+        debug_assert!(neighbors.len() <= degree, "max degree bound violated");
 
         // Post processing saturation if enabled.
         if options.force_saturate || (self.config.saturate_after_prune() && alpha > 1.0f32) {
             for neighbor in context.pool.iter() {
-                if dst.len() >= degree {
+                if neighbors.len() >= degree {
                     break;
                 }
 
                 if !exclude(neighbor.id) {
                     // `AdjacencyList` filters out duplicates. No need to explicitly
                     // check.
-                    dst.push(neighbor.id);
+                    neighbors.push(neighbor.id);
                 }
             }
         }
