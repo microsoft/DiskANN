1a367cb020cee84c6e869706e230e90a9af03fa8
Mark Hildebrand <mhildebrand@microsoft.com>
2026-06-10 11:59:24 -0700

    Checkpoint.

--- diskann-benchmark/src/index/mod.rs
+++ diskann-benchmark/src/index/mod.rs
@@ -5,19 +5,21 @@
 
 use diskann_benchmark_runner::Registry;
 
-mod build;
+pub(crate) mod build;
 mod search;
 mod streaming;
 
 mod benchmarks;
 mod inmem;
 mod result;
+mod inmem2;
 
 #[cfg(feature = "bftree")]
 mod bftree;
 
 pub(crate) fn register_benchmarks(registry: &mut Registry) -> anyhow::Result<()> {
     benchmarks::register_benchmarks(registry)?;
+    inmem2::register_benchmarks(registry)?;
 
     #[cfg(feature = "bftree")]
     bftree::register_benchmarks(registry)?;
