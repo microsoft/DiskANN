4acecee75474bf73b06c88a61a17374927fbdda1
Naren Datha <narendatha@microsoft.com>
2026-06-30 15:35:46 +0530

    Remove redundant index/post_processor module; use inmem::DeterminantDiversity

--- diskann-benchmark/src/index/post_processor/mod.rs
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) Microsoft Corporation.
- * Licensed under the MIT license.
- */
-
-pub(crate) mod determinant_diversity;
-
-pub(crate) use determinant_diversity::DeterminantDiversity;
