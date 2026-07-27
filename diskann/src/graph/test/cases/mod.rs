178871b97cf2751b9aa299c33b5b6d08bc2e9954
Weiyao Luo <9347182+SeliMeli@users.noreply.github.com>
2026-07-27 14:23:34 +0000

    graph: characterize robust prune behavior

--- diskann/src/graph/test/cases/mod.rs
+++ diskann/src/graph/test/cases/mod.rs
@@ -12,6 +12,7 @@ mod inline;
 mod inplace_delete;
 mod multihop;
 mod paged_search;
+mod prune;
 mod range_search;
 
 /// Set to `true` and recompile to include full adjacency list state in participating
