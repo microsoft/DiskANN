35a4e279989ccbb2a7fb5451fa7190157984723a
Mark Hildebrand <mhildebrand@microsoft.com>
2026-08-31 08:23:53 -0700

    Allow inspection of the paged search accessor.

--- diskann/src/graph/search/paged.rs
+++ diskann/src/graph/search/paged.rs
@@ -42,6 +42,19 @@ where
     DP: DataProvider,
     A: SearchAccessor<Id = DP::InternalId> + 'a,
 {
+    /// Return the contained [`SearchAccessor`].
+    ///
+    /// This method is mainly intended to allow per-page metric reporting from the accessor.
+    pub fn accessor(&mut self) -> &mut A {
+        &mut self.accessor
+    }
+}
+
+impl<'a, DP, A> PagedSearch<'a, DP, A>
+where
+    DP: DataProvider,
+    A: SearchAccessor<Id = DP::InternalId> + 'a,
+{
     /// Returns the next page of at most `k` nearest-neighbor results.
     ///
     /// Results across pages are non-overlapping but not guaranteed to be monotonic with
